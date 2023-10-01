0e3fec613f4e2bfb1aa1aa92d04cb04d3451af8a
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-10-01 15:00:57 -0400

    Added Astro-VE Component

---
+++ packages/astro-ve/index.ts
@@ -0,0 +1,4 @@
+// Do not write code directly here, instead use the `src` folder!
+
+// @ts-expect-error TypeScript doesn't know about Astro files
+export { default as AstroVe } from './src/AstroVe.astro';
