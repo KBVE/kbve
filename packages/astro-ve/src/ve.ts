9a045f9105e6d4a73a440af7920473a1db735365
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-10-02 00:56:41 -0400

    Added Nanostores

---
+++ packages/astro-ve/src/ve.ts
@@ -0,0 +1,14 @@
+//              [VE]:VirtualEngine
+//?             [MonoRepo]
+export interface VE$$ {
+    //* Load
+    "load"?: "none" | "lazy" | "partytown" | "helmet";
+    //* VE
+    "ve"?: string;
+    //* Data
+    "data"?: string;
+    //* Widget
+    "widget"?: string;
+
+
+}
