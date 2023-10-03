bbf2ef6329730b2e84c97a5d2a4fe7822b391596
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-10-03 15:20:46 -0400

    Minor Updates - Added Proxy for Main.ts - Added Autogen example config.

--- apps/appwrite-node/src/main.ts
+++ apps/appwrite-node/src/main.ts
@@ -8,9 +8,19 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = new koa();
 
 app.use(async (ctx) => {
-  ctx.body = { message: 'Hello API' };
-});
-
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+	ctx.body = { message: 'Hello API' };
+})
+	.use(
+		proxy('/api', {
+			target: 'https://pb.kbve.com/',
+			changeOrigin: true,
+			//secure: false,
+			//timeout: 300000,
+			//rewrite: path => path.replace(/^\/kbvedatabase(\/|\/\w+)?$/, '/_'),
+			logs: true,
+		})
+	)
+	.use(parser())
+	.listen(port, host, () => {
+		console.log(`[ ready ] http://${host}:${port}`);
+	});
---
+++ apps/atlas/kbve_atlas/atlas_autogen.py
@@ -0,0 +1,27 @@
+import autogen
+
+# Autogen Configruation
+
+config_list = autogen.config_list_from_json("OAI_CONFIG_LIST")
+
+llm_config={
+    "request_timeout": 600,
+    "seed": 42,
+    "config_list": config_list,
+    "temperature": 0,
+}
+
+assistant = autogen.AssistantAgent(
+    name="assistant",
+    llm_config=llm_config,
+)
+
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    human_input_mode="NEVER", 
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={
+        "work_dir": "coding",
+        "use_docker": False,
+    },
+)
