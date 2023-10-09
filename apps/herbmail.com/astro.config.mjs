61ea330910263967f61d1a30d91a41d001d581c1
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-10-09 10:23:13 -0400

    - Added Astro Prefetch - Test case of the AstroVE Headers

--- apps/herbmail.com/astro.config.mjs
+++ apps/herbmail.com/astro.config.mjs
@@ -4,16 +4,21 @@ import svelte from '@astrojs/svelte';
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import prefetch from '@astrojs/prefetch';
 import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://herbmail.com/',
   outDir: '../../dist/apps/herbmail.com',
   integrations: [
     react(),
     svelte(),
     partytown(),
     sitemap(),
+    prefetch({
+			throttle: 5,
+		}),
     tailwind({
       configFile: fileURLToPath(
         new URL('./tailwind.config.cjs', import.meta.url)
