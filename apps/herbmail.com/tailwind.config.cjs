ff4bcb57ba78855d6740ca820b09b74ef8ed88b2
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-10-08 20:22:30 -0400

    Init. Migration

--- apps/herbmail.com/tailwind.config.cjs
+++ apps/herbmail.com/tailwind.config.cjs
@@ -19,6 +19,25 @@ module.exports = {
         'herbmail': {
           DEFAULT: '#8C52FF',
         },
+        primary: "",
+        secondary: "",
+
+      },
+      backgroundColor: {
+        default: "var(--color-background)",
+        //offset: "var(--color-background-offset)",
+        offset: "#23262d",
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translate3d(0, 0, 0)' },
+          '50%': { transform: 'translate3d(0, 30px, 0)' },
+        },
+        'ltr-linear-infinite': {
+          '0%, 100%': { 'background-position': '0 0' },
+          '50%': { 'background-position': '400% 0%' },
+        },
+       
       },
     },
   },
