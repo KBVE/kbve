a19bc11caf24e8a20e37392fb3bbd73177bf2548
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-10-24 22:04:31 -0400

    Added :  twin.macro, babel-plugin-marcos. Preparing the React-Appwrite library for usage in AstroJS

--- packages/react-appwrite/src/lib/react-appwrite.tsx
+++ packages/react-appwrite/src/lib/react-appwrite.tsx
@@ -1,18 +1,49 @@
-import styled from 'styled-components';
+//import tw from 'twin.macro';
 
 /* eslint-disable-next-line */
 export interface ReactAppwriteProps {}
 
-const StyledReactAppwrite = styled.div`
-  color: pink;
-`;
-
 export function ReactAppwrite(props: ReactAppwriteProps) {
-  return (
-    <StyledReactAppwrite>
-      <h1>Welcome to ReactAppwrite!</h1>
-    </StyledReactAppwrite>
-  );
+	return (
+		<>
+			<div className="px-4 py-3">
+				<span className="block text-sm text-white">Guest</span>
+				<span className="block text-sm truncate text-zinc-400">
+					hi@kbve.com
+				</span>
+			</div>
+			<ul className="py-2" aria-labelledby="user-menu-button">
+				<li>
+					<a
+						href="/#"
+						className="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
+						Dashboard
+					</a>
+				</li>
+				<li>
+					<a
+						href="/#"
+						className="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
+						Settings
+					</a>
+				</li>
+				<li>
+					<a
+						href="/#"
+						className="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
+						Earnings
+					</a>
+				</li>
+				<li>
+					<a
+						href="/#"
+						className="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
+						Sign out
+					</a>
+				</li>
+			</ul>
+		</>
+	);
 }
 
 export default ReactAppwrite;
