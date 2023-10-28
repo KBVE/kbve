28bb7c7a929541ad7222e444f8a0b1a631da848b
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-10-27 20:08:01 -0400

    Take in the whole sentences for the patch.

--- scripts/atomic.sh
+++ scripts/atomic.sh
@@ -10,8 +10,9 @@ GIT_DATE=$(date +'%m-%d-%Y-%s')
 if [ "$#" -eq "0" ]; then
   PATCH_NAME="patch-atomic-${GIT_DATE}"
 else
-  UNFORMAT_PATCH="${1}"
-  NEW_PATCH=$(echo "$UNFORMAT_PATCH" | sed 's/[^[:alnum:]]/-/g')
+  UNFORMAT_PATCH=$(echo "$@" | tr ' ' '-')
+  NEW_PATCH="${UNFORMAT_PATCH//[^[:alnum:]-]/-}"
+  NEW_PATCH="${NEW_PATCH,,}"
   PATCH_NAME="patch-atomic-${NEW_PATCH}-${GIT_DATE}"
 fi
 
