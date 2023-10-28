bb6e8b013a2f8e96e1fd59e756a0b0cc7adf408a
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-10-27 20:01:49 -0400

    Cleaning up the shell file and trying the sed.

--- scripts/atomic.sh
+++ scripts/atomic.sh
@@ -1,16 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-#   Switch to Dev Branch
 git switch dev
 
-#   Git Pull
 git pull
-
-#   Checkout Origin Dev Branch
-#git checkout origin/dev
-
-#   Switch to Atomic Patch Branch
 
 GIT_DATE=$(date +'%m-%d-%Y-%s')
 
@@ -18,11 +11,8 @@ if [ "$#" -eq "0" ]; then
   PATCH_NAME="patch-atomic-${GIT_DATE}"
 else
   UNFORMAT_PATCH="${1}"
-  NEW_PATCH="${UNFORMAT_PATCH//[^[:alnum:]]/-}"
+  NEW_PATCH=$(echo "$UNFORMAT_PATCH" | sed 's/[^[:alnum:]]/-/g')
   PATCH_NAME="patch-atomic-${NEW_PATCH}-${GIT_DATE}"
 fi
 
 git switch -c "${PATCH_NAME}"
-
-
-#   git switch -c "patch-atlas-${git_date}"
