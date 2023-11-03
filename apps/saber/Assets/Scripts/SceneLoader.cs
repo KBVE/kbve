d5afffcee34c57b92fa1f3599526c5e40573789c
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-11-03 18:51:50 -0400

    Update SceneLoader.cs
    Issue: https://github.com/KBVE/kbve/issues/166

--- apps/saber/Assets/Scripts/SceneLoader.cs
+++ apps/saber/Assets/Scripts/SceneLoader.cs
@@ -1,33 +1,74 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneLoader : MonoBehaviour
 {
-  public Slider progressBar;  // Reference to the UI Slider component
+    private Slider progressBar;
 
-  public void LoadSceneAsync(string sceneName)
-  {
-    StartCoroutine(LoadYourAsyncScene(sceneName));
-  }
-
-  private System.Collections.IEnumerator LoadYourAsyncScene(string sceneName)
-  {
-    AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
-    asyncLoad.allowSceneActivation = false;  // Don't allow the scene to activate until loading is complete
-
-    while (!asyncLoad.isDone)
+    void Start()
     {
-      // Update the progress bar value
-      progressBar.value = Mathf.Clamp01(asyncLoad.progress / 0.9f);
-
-      // Check if the loading is complete and ready to activate the scene
-      if (asyncLoad.progress >= 0.9f)
-      {
-        asyncLoad.allowSceneActivation = true;  // Allow the scene to activate
-      }
-
-      yield return null;
+        CreateProgressBar();
     }
-  }
+
+    private void CreateProgressBar()
+    {
+        // Create a new Canvas GameObject
+        GameObject canvasGameObject = new GameObject("Canvas");
+        canvasGameObject.layer = LayerMask.NameToLayer("UI");
+        Canvas canvas = canvasGameObject.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.WorldSpace;
+        CanvasScaler cs = canvasGameObject.AddComponent<CanvasScaler>();
+        cs.scaleFactor = 10.0f;
+        cs.dynamicPixelsPerUnit = 10f;
+        canvasGameObject.AddComponent<GraphicRaycaster>();
+
+        // Create a new Slider GameObject
+        GameObject sliderGameObject = new GameObject("Slider");
+        sliderGameObject.transform.SetParent(canvasGameObject.transform);
+        sliderGameObject.transform.localPosition = Vector3.zero;
+        progressBar = sliderGameObject.AddComponent<Slider>();
+
+        // Set up the slider values
+        progressBar.minValue = 0;
+        progressBar.maxValue = 1;
+        progressBar.value = 0;
+
+        // Optionally add visual elements to your slider here
+
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    private IEnumerator LoadSceneAsync(string sceneName)
+    {
+        // Set the progress bar active
+        progressBar.gameObject.SetActive(true);
+
+        // Start async loading
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        asyncOperation.allowSceneActivation = false;
+
+        // Update progress bar
+        while (!asyncOperation.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncOperation.progress / 0.9f);
+            progressBar.value = progress;
+
+            // Check if loading is finished
+            if (asyncOperation.progress >= 0.9f)
+            {
+                asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+
+        // Hide the progress bar
+        progressBar.gameObject.SetActive(false);
+    }
 }
