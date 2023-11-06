7fe80ae685471622237c239cb3e91ef1deeb1661
Mert Karaca <38283518+sjuhyeon@users.noreply.github.com>
2023-11-06 13:09:15 +0300

    Improved PlayerInteract.cs

--- apps/saber/Assets/Scripts/Interactions/PlayerInteract.cs
+++ apps/saber/Assets/Scripts/Interactions/PlayerInteract.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
-    [Tooltip("The Transform representing the player's position.")]
-    [SerializeField] Transform playerPos;
+    [Tooltip("The transform representing the position for sphere-based interaction checks.")]
+    [SerializeField] Transform sphereCheck;
 
     [Tooltip("Maximum distance for object interaction")]
-    [SerializeField] float interactionDistance = 3f;
+    [SerializeField] float interactionDistance = 9f;
 
     [Tooltip("Layer(s) for interactable objects")]
     [SerializeField] LayerMask interactableLayer;
@@ -14,45 +14,65 @@ public class PlayerInteract : MonoBehaviour
     [Tooltip("Key for interaction")]
     [SerializeField] KeyCode interactionKey = KeyCode.E;
 
+    [Space(5)]
+    // Debug options
     [SerializeField] bool onDebug = true;
+    [SerializeField] Color sphereDebugColor = Color.blue;
 
     private Ray ray; // Represents the interaction ray.
     private RaycastHit raycastHit; // Stores information about what the ray hits.
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
     void Update()
     {
         // Generate the interaction ray and display it for debugging.
-        GenerateRay();
-        DisplayRayOnDebug(onDebug);
+        CheckInteractableHits();
     }
 
-    void GenerateRay()
+    void CheckInteractableHits()
     {
-        // Create a ray from the player's position in the direction they are facing.
-        ray = new Ray(playerPos.transform.position, playerPos.transform.forward);
+        // Check if the sphere intersects with any objects on the specified layer within the interaction distance.
+        bool foundObj = Physics.CheckSphere(sphereCheck.position, interactionDistance, interactableLayer);
 
-        // Check if the ray hits an interactable object within the specified distance and layer.
-        if (Physics.Raycast(ray, out raycastHit, interactionDistance, interactableLayer))
+
+        if (foundObj)
         {
-            Interactable interact = raycastHit.collider.GetComponent<Interactable>();
+            // Get all colliders that are within the specified sphere.
+            Collider[] cols = Physics.OverlapSphere(sphereCheck.position, interactionDistance, interactableLayer);
 
-            // If an Interactable component is found and the interaction key is pressed, trigger interaction.
-            if (interact != null)
+            // Iterate through the colliders found within the sphere.
+            foreach (var col in cols)
             {
-                if (Input.GetKeyDown(interactionKey))
-                {
-                    interact.BaseInteract(); // Call the interaction method on the Interactable component.
-                }
+                // Check if the collider has an Interactable component.
+                Interactable interactable = col.GetComponent<Interactable>();
 
-                UpdateUIPromptMessage(interact.promptMsg);
+                if (interactable != null)
+                {
+                    // Check if the interaction key is pressed and trigger the interaction.
+                    if (Input.GetKeyDown(interactionKey))
+                    {
+                        interactable.BaseInteract();
+                    }
+
+                    // Update the UI prompt message for the interactable object.
+                    UpdateUIPromptMessage(interactable.promptMsg);
+                }
             }
         }
     }
 
-    void DisplayRayOnDebug(bool showRay)
+    void OnDrawGizmosSelected()
     {
-        // If debugging is enabled, draw a visible ray for debugging purposes.
-        if (showRay)
-            Debug.DrawRay(ray.origin, ray.direction * interactionDistance);
+        if (onDebug)
+        {
+            // Draw a sphere at the decided position to visualize the interaction distance.
+            Gizmos.color = sphereDebugColor;
+            Gizmos.DrawWireSphere(sphereCheck.position, interactionDistance);
+        }
     }
 
     void UpdateUIPromptMessage(string promptMsg)
