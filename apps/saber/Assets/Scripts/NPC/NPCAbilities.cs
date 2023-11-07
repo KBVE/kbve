1c8365d27dee6f031ad96dd0dab3bfb5cd8453d7
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-11-07 05:10:28 -0500

    Updated NPC Ghost Prefab
    - Under the HolyByte.scene - It should now render the ghost, its healthbar and follow the player when the ghost is spawn into the game.

--- apps/saber/Assets/Scripts/NPC/NPCAbilities.cs
+++ apps/saber/Assets/Scripts/NPC/NPCAbilities.cs
@@ -14,6 +14,20 @@ public class NPCAbilities : MonoBehaviour
     private float initialYPosition;
     private bool isInitialYPositionSet = false;
 
+
+    public Transform target; // The target that the NPC will follow
+    public float followDistance = 1.0f; // The distance the NPC will keep from the target
+    public float followSpeed = 5.0f; // Speed at which the NPC will follow the target
+
+
+    private void Update()
+    {
+        if (target != null) // Only follow if there is a target assigned
+        {
+            FollowTarget();
+        }
+    }
+
     public void Bobbing(Transform transform)
     {
         if (!isInitialYPositionSet)
@@ -30,6 +44,19 @@ public class NPCAbilities : MonoBehaviour
     {
         renderer.material.color = new Color(1, 1, 1, transparency);
     }
+
+    private void FollowTarget()
+    {
+        Vector3 directionToTarget = (target.position - transform.position).normalized;
+        Vector3 desiredPosition = target.position - directionToTarget * followDistance;
+
+        // Ensure the NPC only moves along the x and z axes (assuming y is up/down)
+        desiredPosition.y = transform.position.y;
+
+        // Smoothly interpolate to the desired position
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, followSpeed * Time.deltaTime);
+    }
+
 
 
 }
