62896b8e1885b49df7390a5251d5f0116ca0dd46
pablowunderlich <124636893+pablowunderlich@users.noreply.github.com>
2023-11-10 15:58:34 -0600

    #163
    Added a ton of sfx and new script for managing sfx.

---
+++ apps/saber/Assets/Scripts/AudioScripts/PlayerFX.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFX : MonoBehaviour
+{
+  private AudioSource _dragonSource;
+  [SerializeField] AudioClip[] jump;
+  [SerializeField] AudioClip[] wingFlap;
+  [SerializeField] AudioClip[] attack;
+  [SerializeField] AudioClip[] land;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
