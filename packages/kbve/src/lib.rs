40e4f90f610db384419948f46476d2500fc20c7e
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-11-18 05:33:38 -0500

    Started the base foundation for the player module

--- packages/kbve/src/lib.rs
+++ packages/kbve/src/lib.rs
@@ -3,9 +3,11 @@ pub mod db;
 pub mod models;
 pub mod schema;
 pub mod utils;
+pub mod player;
 
 // *  [USE]
 pub use db::*;
 pub use models::*;
 pub use schema::*;
 pub use utils::*;
+pub use player::*;
