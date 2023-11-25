c67f3f2dba2e57c4b74f2fe8f4e71c51ee9e6ac8
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-11-25 15:17:58 -0500

    Preparing the email sanitizing with lazy regex

--- packages/kbve/src/dbms/wh.rs
+++ packages/kbve/src/dbms/wh.rs
@@ -1,35 +1,26 @@
 use serde::Serialize;
 
-
 #[derive(Serialize)]
 pub struct WizardResponse {
-    pub data: String,
-    pub message: String,
+	pub data: String,
+	pub message: String,
 }
-
-#[derive(Serialize)]
-pub struct FallbackResponse {
-    pub message: String,
-    pub path: String,
-}
-
 
 #[derive(Serialize)]
 pub struct UserResponse {
 	pub id: u64,
 	pub username: String,
-    pub role: i32,
-    pub reputation: i32,
-    pub exp: i32,
+	pub role: i32,
+	pub reputation: i32,
+	pub exp: i32,
 }
-
 
 #[derive(Serialize)]
 pub struct ProfileResponse {
 	pub name: String,
 	pub bio: String,
-    pub unsplash: String,
-    pub github: String,
-    pub instagram: String,
-    pub discord: String,
+	pub unsplash: String,
+	pub github: String,
+	pub instagram: String,
+	pub discord: String,
 }
