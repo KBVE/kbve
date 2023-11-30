be3455a81eec974fb08baed6ee8add39e23494fd
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-11-29 20:18:03 -0500

    Update NPC.cs
    Added MainCamera as a reference over Camera.main

--- apps/saber/Assets/Scripts/NPC/NPC.cs
+++ apps/saber/Assets/Scripts/NPC/NPC.cs
@@ -30,7 +30,7 @@ public class NPC : Entity
         Debug.LogError("SpriteRenderer component is missing on this GameObject.", this);
       }
     }
-    if (Camera.main == null)
+    if (MainCamera == null)
     {
       if (this.DebugMode)
       {
@@ -72,14 +72,14 @@ public class NPC : Entity
 
   protected virtual void Update()
   {
-    if (spriteRenderer == null || Camera.main == null)
+    if (spriteRenderer == null || MainCamera == null)
     {
       return;
     }
 
     this.Position = transform.position; // Update location each frame
 
-    transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+    transform.rotation = Quaternion.Euler(0f, MainCamera.transform.rotation.eulerAngles.y, 0f);
 
     FlipSpriteBasedOnViewpoint();
   }
@@ -90,7 +90,7 @@ public class NPC : Entity
 
   private void FlipSpriteBasedOnViewpoint()
   {
-    Vector3 toCamera = Camera.main.transform.position - transform.position;
+    Vector3 toCamera = MainCamera.transform.position - transform.position;
     bool shouldFlip = Vector3.Dot(toCamera, transform.right) < 0f;
     spriteRenderer.flipX = shouldFlip;
   }
