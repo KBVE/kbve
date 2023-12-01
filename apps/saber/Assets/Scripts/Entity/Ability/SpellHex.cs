d8bc5c731dd8ddbd6852f122a67f21b2d8e34d3b
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-12-01 16:28:02 -0500

    Unity Saber Ability Integration
    Passing the LayerMask into the SpellHex

--- apps/saber/Assets/Scripts/Entity/Ability/SpellHex.cs
+++ apps/saber/Assets/Scripts/Entity/Ability/SpellHex.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class SpellHex : MonoBehaviour
 {
-    private float radius; // Radius of the AoE
-    public float duration; // Duration of the effect
-    public LayerMask affectedLayers; // Layers affected by the spell
+    private Entity casterEntity;
     private int damageAmount;
-    public float damageInterval = 1f; // Interval in seconds to apply damage
+    private float radius; // Radius of the AoE
+    private float duration; // Duration of the effect
+    private float damageInterval = 1f; // Interval in seconds to apply damage
     private float lastDamageTime;
 
-    public void Initialize(int damage, float dur, float rad)
+    public void Initialize(Entity caster, int damage, float dur, float rad)
     {
+        casterEntity = caster;
         damageAmount = damage;
         duration = dur;
         radius = rad;
@@ -50,7 +51,7 @@ public class SpellHex : MonoBehaviour
     private void ApplyEffect(Collider other)
     {
         // Check if the collider's layer is in the affectedLayers
-        if (((1 << other.gameObject.layer) & affectedLayers) != 0)
+        if (((1 << other.gameObject.layer) & casterEntity.PlayerLayer) != 0)
         {
             Entity entity = other.GetComponent<Entity>();
             if (entity != null)
