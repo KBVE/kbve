f4a376a018659acad568556e97114c34648a05fc
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-12-15 09:38:31 -0500

    - Removed some minor css

--- packages/astro-ve/src/lib/kbve/register/Register.svelte
+++ packages/astro-ve/src/lib/kbve/register/Register.svelte
@@ -229,7 +229,7 @@
 								name="confirm"
 								id="confirm"
 								placeholder="••••••••"
-								class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
 								required
 								bind:value={confirm} />
 						</div>
@@ -243,7 +243,7 @@
 						</div>
 						<button
 							type="submit"
-							class="w-full bg-secondary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+							class="w-full bg-secondary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
 							disabled={loading}
 							><span>{loading ? 'Loading' : 'Login'}</span></button>
 						<p class="text-sm font-light text-primary">
