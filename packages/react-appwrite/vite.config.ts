5fa8ff998b8fa90db087470b13a1a8d315f5df2d
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-12-15 07:53:01 -0500

    Migrating NX

--- packages/react-appwrite/vite.config.ts
+++ packages/react-appwrite/vite.config.ts
@@ -5,46 +5,55 @@ import * as path from 'path';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/react-appwrite',
+	root: __dirname,
+	cacheDir: '../../node_modules/.vite/react-appwrite',
 
-  plugins: [
-    react(),
-    nxViteTsPaths(),
-    dts({
-      entryRoot: 'src',
-      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
-      skipDiagnostics: true,
-    }),
-  ],
+	plugins: [
+		react(),
+		nxViteTsPaths(),
+		dts({
+			entryRoot: 'src',
+			tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
+			skipDiagnostics: true,
+		}),
+	],
 
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
+	// Uncomment this if you are using workers.
+	// worker: {
+	//  plugins: [ nxViteTsPaths() ],
+	// },
 
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
-  build: {
-    entry: 'src/index.ts',
-    name: 'react-appwrite',
-    fileName: 'index',
-    formats: ['es', 'cjs'],
-    external: ['react', 'react-dom', 'react/jsx-runtime'],
-    lib: {
-      entry: 'src/index.ts',
-      name: 'react-appwrite',
-      fileName: 'index',
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      external: ["'react'", "'react-dom'", "'react/jsx-runtime'"],
-    },
-  },
+	// Configuration for building your library.
+	// See: https://vitejs.dev/guide/build.html#library-mode
+	build: {
+		outDir: '../../dist/packages/react-appwrite',
+		reportCompressedSize: true,
+		commonjsOptions: { transformMixedEsModules: true },
+		entry: 'src/index.ts',
+		name: 'react-appwrite',
+		fileName: 'index',
+		formats: ['es', 'cjs'],
+		external: ['react', 'react-dom', 'react/jsx-runtime'],
+		lib: {
+			entry: 'src/index.ts',
+			name: 'react-appwrite',
+			fileName: 'index',
+			formats: ['es', 'cjs'],
+		},
+		rollupOptions: {
+			external: ["'react'", "'react-dom'", "'react/jsx-runtime'"],
+		},
+	},
 
-  test: {
-    globals: true,
-    cache: { dir: '../../node_modules/.vitest' },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
+	test: {
+		reporters: ['default'],
+		coverage: {
+			reportsDirectory: '../../coverage/packages/react-appwrite',
+			provider: 'v8',
+		},
+		globals: true,
+		cache: { dir: '../../node_modules/.vitest' },
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+	},
 });
