13a8ab17ee1ede2eec5d513d55350849498ac9af
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-12-20 22:23:24 -0500

    Grabbing data from the locker

--- packages/astro-ve/src/lib/kbve/user/UserMenu.svelte
+++ packages/astro-ve/src/lib/kbve/user/UserMenu.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 
+    import { kbve$ } from '../../storage';
+
 </script>
+
 
 <div class="px-4 py-3">
     <span class="block text-sm text-white">Guest</span>
     <span class="block text-sm truncate text-zinc-400">
-        hi@kbve.coms
+        {$kbve$.email}
     </span>
 </div>
 <ul class="py-2" aria-labelledby="user-menu-button">
@@ -13,7 +16,7 @@
         <a
             href="/#"
             class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
-            Dashboards
+            Dashboard
         </a>
     </li>
     <li>
