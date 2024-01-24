00bc4a7d24b027b7f9ee2dab3e0b7995f9e80fac
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-01-24 04:45:02 -0500

    Herbmail WASM Test Case

--- apps/herbmail.com/astro.config.mjs
+++ apps/herbmail.com/astro.config.mjs
@@ -5,12 +5,18 @@ import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import prefetch from '@astrojs/prefetch';
+import mdx from '@astrojs/mdx';
+
 import { fileURLToPath } from 'node:url';
+import markdownConfig from './markdown.config';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://herbmail.com/',
   outDir: '../../dist/apps/herbmail.com',
+  markdown: markdownConfig,
+
   integrations: [
     react(),
     svelte(),
@@ -18,6 +24,10 @@ export default defineConfig({
     sitemap(),
     prefetch({
 			throttle: 5,
+		}),
+    mdx({
+			...markdownConfig,
+			//extendPlugins: "astroDefaults"
 		}),
     tailwind({
       configFile: fileURLToPath(
