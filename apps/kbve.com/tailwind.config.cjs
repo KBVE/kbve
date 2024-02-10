371be0243911c2d9182ef9d777a27a67994ea78e
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-02-10 03:09:35 -0500

    Minor Tweaks
    Just some minor updates to the site while test casing with the macbook.

--- apps/kbve.com/tailwind.config.cjs
+++ apps/kbve.com/tailwind.config.cjs
@@ -8,12 +8,49 @@ module.exports = {
   content: [
     join(
       __dirname,
-      'src/**/!(*.stories|*.spec).{astro,html,js,jsx,md,svelte,ts,tsx,vue}'
+      'src/**/!(*.stories|*.spec).{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+        // Color extension is prefixed as KBVE to avoid class conflicts
+        'kbve':  '#8C52FF',
+      
+        'kbve-primary':  "#48BB78",
+        'kbve-primary-light': "#48BB78",
+        
+        'kbve-secondary': "#1c033c",
+
+        'kbve-menu-primary': "#27272A",
+
+        
+        
+      },
+      backgroundColor: {
+        default: "var(--color-background)",
+        //offset: "var(--color-background-offset)",
+        offset: "#23262d",
+      },
+
+      backgroundImage: theme => ({
+        'custom-gradient': `linear-gradient(to right, ${theme('colors.kbve')}, ${theme('colors.kbve-primary')}, ${theme('colors.kbve-secondary')})`,
+      }),
+
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translate3d(0, 0, 0)' },
+          '50%': { transform: 'translate3d(0, 30px, 0)' },
+        },
+        'ltr-linear-infinite': {
+          '0%, 100%': { 'background-position': '0 0' },
+          '50%': { 'background-position': '400% 0%' },
+        },
+       
+      },
+    },
   },
   plugins: [],
 };
