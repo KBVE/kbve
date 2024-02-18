f32390140664e33e1ce3c96b73f2c8f8f676b9f0
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-02-17 19:33:09 -0500

    Drawer Sync before adding dynamics.

--- apps/kbve.com/tailwind.config.cjs
+++ apps/kbve.com/tailwind.config.cjs
@@ -1,56 +1,58 @@
 const {
-  createGlobPatternsForDependencies,
+	createGlobPatternsForDependencies,
 } = require('@nxtensions/astro/tailwind');
 const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(
-      __dirname,
-      'src/**/!(*.stories|*.spec).{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    extend: {
+	content: [
+		join(
+			__dirname,
+			'src/**/!(*.stories|*.spec).{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+		),
+		...createGlobPatternsForDependencies(__dirname),
+	],
+	theme: {
+		extend: {
+			colors: {
+				// Color extension is prefixed as KBVE to avoid class conflicts
+				kbve: '#8C52FF',
 
-      colors: {
-        // Color extension is prefixed as KBVE to avoid class conflicts
-        'kbve':  '#8C52FF',
-      
-        'kbve-primary':  "#48BB78",
-        'kbve-primary-light': "#48BB78",
-        
-        'kbve-secondary': "#1c033c",
+				'kbve-primary': '#48BB78',
+				'kbve-primary-light': '#48BB78',
 
-        'kbve-menu-primary': "#27272A",
+				'kbve-secondary': '#1c033c',
 
-        
-        
-      },
-      backgroundColor: {
-        default: "var(--color-background)",
-        //offset: "var(--color-background-offset)",
-        offset: "#23262d",
-      },
+				'kbve-menu-primary': '#27272A',
+        'kbve-svg-primary': '',
 
-      backgroundImage: theme => ({
-        'custom-gradient': `linear-gradient(to right, ${theme('colors.kbve')}, ${theme('colors.kbve-primary')}, ${theme('colors.kbve-secondary')})`,
-      }),
+			},
+			backgroundColor: {
+				default: 'var(--color-background)',
+				//offset: "var(--color-background-offset)",
+				'kbve-menu-bg': '#09090b', // zinc 950 as hex
+				offset: '#23262d',
+			},
 
-      keyframes: {
-        float: {
-          '0%, 100%': { transform: 'translate3d(0, 0, 0)' },
-          '50%': { transform: 'translate3d(0, 30px, 0)' },
-        },
-        'ltr-linear-infinite': {
-          '0%, 100%': { 'background-position': '0 0' },
-          '50%': { 'background-position': '400% 0%' },
-        },
-       
-      },
-    },
-  },
-  plugins: [],
+			backgroundImage: (theme) => ({
+				'custom-gradient': `linear-gradient(to right, ${theme(
+					'colors.kbve'
+				)}, ${theme('colors.kbve-primary')}, ${theme(
+					'colors.kbve-secondary'
+				)})`,
+			}),
+
+			keyframes: {
+				float: {
+					'0%, 100%': { transform: 'translate3d(0, 0, 0)' },
+					'50%': { transform: 'translate3d(0, 30px, 0)' },
+				},
+				'ltr-linear-infinite': {
+					'0%, 100%': { 'background-position': '0 0' },
+					'50%': { 'background-position': '400% 0%' },
+				},
+			},
+		},
+	},
+	plugins: [],
 };
