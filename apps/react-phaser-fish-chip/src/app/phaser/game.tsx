c054277d9d49114897fbdac7bf4bb017ad8d713f
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-03-08 18:34:47 -0500

    Starting the Phaser integration

---
+++ apps/react-phaser-fish-chip/src/app/phaser/game.tsx
@@ -0,0 +1,18 @@
+
+import styled from 'styled-components';
+
+
+const StyledApp = styled.div`
+    // Your style here
+`;
+
+
+export function Game() {
+    return (
+        <StyledApp>
+
+        </StyledApp>
+    )
+}
+
+export default Game;
