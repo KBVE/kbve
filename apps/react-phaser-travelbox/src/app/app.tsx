00711ab1a770b3f3197a10ba447e17ea7494a5ac
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-03-15 03:43:04 -0400

    TravelBox Init.

---
+++ apps/react-phaser-travelbox/src/app/app.tsx
@@ -0,0 +1,26 @@
+
+import styled from 'styled-components';
+
+import NxWelcome from "./nx-welcome";
+
+
+const StyledApp = styled.div`
+    // Your style here
+`;
+
+
+export function App() {
+
+  return (
+      <StyledApp>
+        
+        <NxWelcome title="react-phaser-travelbox"/>
+        
+      </StyledApp>
+  );
+
+}
+
+
+export default App;
+
