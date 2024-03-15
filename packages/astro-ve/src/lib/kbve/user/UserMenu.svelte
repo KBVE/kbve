765442d31b2e7d5b6b8da705e7963564c1cda49d
J. Zane Cook <jzanecook@z90.studio>
2024-03-14 20:26:53 -0500

    fix(kbve.com): Changed Header Menu
    - Added if statement for login button as placeholder
    - Replaced lottie menu icon with the swipe icon for consistency
    - Removed unused elements
    - Added svg icons

--- packages/astro-ve/src/lib/kbve/user/UserMenu.svelte
+++ packages/astro-ve/src/lib/kbve/user/UserMenu.svelte
@@ -4,40 +4,58 @@
 
 </script>
 
-
-<div class="px-4 py-3">
-    <span class="block text-sm text-white">Guest</span>
-    <span class="block text-sm truncate text-zinc-400">
-        {$kbve$.email}
-    </span>
-</div>
-<ul class="py-2" aria-labelledby="user-menu-button">
-    <li>
-        <a
-            href="/#"
-            class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
-            Dashboard
-        </a>
-    </li>
-    <li>
-        <a
-            href="/#"
-            class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
-            Settings
-        </a>
-    </li>
-    <li>
-        <a
-            href="/#"
-            class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
-            Earnings
-        </a>
-    </li>
-    <li>
-        <a
-            href="/#"
-            class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
-            Sign out
-        </a>
-    </li>
-</ul>
+{#if $kbve$.email !== undefined}
+    <div class="px-4 py-3">
+        <span class="block text-sm text-white">Guest</span>
+        <span class="block text-sm truncate text-zinc-400">
+            {$kbve$.email}
+        </span>
+    </div>
+    <ul class="py-2" aria-labelledby="user-menu-button">
+        <li>
+            <a
+                href="/#"
+                class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
+                Dashboard
+            </a>
+        </li>
+        <li>
+            <a
+                href="/#"
+                class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
+                Settings
+            </a>
+        </li>
+        <li>
+            <a
+                href="/#"
+                class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
+                Earnings
+            </a>
+        </li>
+        <li>
+            <a
+                href="/#"
+                class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
+                Sign out
+            </a>
+        </li>
+    </ul>
+{:else}
+    <ul class="py-2" aria-labelledby="user-menu-button">
+        <li>
+            <a
+                href="/#"
+                class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
+                Register
+            </a>
+        </li>
+        <li>
+            <a
+                href="/#"
+                class="block px-4 py-2 text-sm hover:bg-zinc-600 text-zinc-200 hover:text-white">
+                Sign in
+            </a>
+        </li>
+    </ul>
+{/if}
