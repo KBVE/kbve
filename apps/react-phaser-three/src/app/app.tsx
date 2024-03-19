da03c996316dc2be3acf0be19d8f67b6149ffe39
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-03-19 09:28:37 -0400

    Initlizing Three Phaser

---
+++ apps/react-phaser-three/src/app/app.tsx
@@ -0,0 +1,26 @@
+
+import styled from 'styled-components';
+
+import NxWelcome from "./nx-welcome";
+
+
+const StyledApp = styled.div`
+    // Your style here
+`;
+
+
+export function App() {
+
+  return (
+      <StyledApp>
+        
+        <NxWelcome title="react-phaser-three"/>
+        
+      </StyledApp>
+  );
+
+}
+
+
+export default App;
+
