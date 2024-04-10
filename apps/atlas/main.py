5deb7c31d1f0ae798b391f138f96dd9a3e15fcac
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-04-10 11:46:00 -0400

    RSS Modules Added

--- apps/atlas/main.py
+++ apps/atlas/main.py
@@ -2,6 +2,8 @@ import asyncio
 from kbve_atlas.api.clients.poetry_db_client import PoetryDBClient
 from kbve_atlas.api.clients.websocket_echo_client import WebsocketEchoClient
 from kbve_atlas.api.clients.coindesk_client import CoinDeskClient
+from kbve_atlas.api.rss.rss_utils import RSSUtility
+
 
 async def poetry_main():
     poetry_client = PoetryDBClient()
@@ -16,6 +18,7 @@ async def poetry_main():
 
         pass
 
+
 async def wss_main():
     websocket_client = WebsocketEchoClient()
     try:
@@ -24,8 +27,9 @@ async def wss_main():
         await websocket_client.close()
 
         pass
-    
-async def coindesk_main():    
+
+
+async def coindesk_main():
     coindesk_client = CoinDeskClient()
     try:
         bitcoin_price = await coindesk_client.get_current_bitcoin_price()
@@ -33,14 +37,32 @@ async def coindesk_main():
     finally:
         await coindesk_client.close()
 
-
         pass
-    
+
+
+async def google_main():
+    #rss_url = "https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en"
+    rss_url = "https://feeds.bbci.co.uk/news/world/rss.xml"
+    rss_utility = RSSUtility(base_url=rss_url)
+
+    try:
+
+        soup = await rss_utility.fetch_and_parse_rss()
+        rss_feed_model = await rss_utility.convert_to_model(soup)
+        formatted_feed = RSSUtility.format_rss_feed(rss_feed_model)
+        print(formatted_feed)
+    finally:
+        print("Done RSS")
+
+    pass
+
+
 async def main():
     # Using asyncio.gather to run both tasks concurrently
     await asyncio.gather(
-        poetry_main(),
-        wss_main(),
+        # poetry_main(),
+        # wss_main(),
+        google_main(),
         coindesk_main(),
     )
 
