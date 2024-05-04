5901533498aae569db41a229c52ce2f314503615
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-05-04 02:18:02 -0400

    Nanostore Update
    Minor Dashboard Changes + Nanostores Update

--- apps/kbve.com/src/components/tooling/dashboard/DashboardClass.jsx
+++ apps/kbve.com/src/components/tooling/dashboard/DashboardClass.jsx
@@ -1,45 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Responsive, WidthProvider } from 'react-grid-layout';
+import { useStore } from '@nanostores/react';
+import { layoutStore$, updateLayout } from '@kbve/khashvault';
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
-class MyGridLayout extends Component {
-  render() {
-    const layout = [
-      { i: 'a', x: 0, y: 0, w: 4, h: 4 },
-      { i: 'b', x: 4, y: 0, w: 4, h: 4 },
-      { i: 'c', x: 8, y: 0, w: 4, h: 4 },
-      { i: 'd', x: 0, y: 0, w: 4, h: 4 },
-      { i: 'e', x: 4, y: 0, w: 4, h: 4 },
-      { i: 'f', x: 0, y: 0, w: 4, h: 4 },
-      { i: 'g', x: 4, y: 0, w: 4, h: 4 },
-      { i: 'h', x: 4, y: 0, w: 4, h: 4 },
-      { i: 'i', x: 0, y: 0, w: 4, h: 4 },
-      { i: 'j', x: 12, y: 0, w: 4, h: 12 }
-    ];
+const MyGridLayout = () => {
+  const layout = useStore(layoutStore$);
 
-    return (
-      <ResponsiveGridLayout className="layout"
-                            layouts={{ lg: layout }}
-                            breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
-                            cols={{lg: 12, md: 10, sm: 6, xs: 4, xxs: 2}}
-                            rowHeight={30}
-                            containerPadding={[10, 10]}
-                            verticalCompact={true}
-                            compactType="null"
-                            isDraggable={true}
-                            isResizable={true}
-                            autoSize={true}
-                            measureBeforeMount={false}
-                            preventCollision={true}>
-        {layout.map(item => (
-          <div key={item.i} className="bg-gray-200 dark:bg-gray-400 border-2 border-gray-400 p-4 shadow" id={`block-${item.i}`}>
-            Block {item.i.toUpperCase()}
-          </div>
-        ))}
-      </ResponsiveGridLayout>
-    );
-  }
-}
+  const handleLayoutChange = (newLayout) => {
+    updateLayout(newLayout);
+  };
+
+  return (
+    <ResponsiveGridLayout
+      className="layout"
+      layouts={{ lg: layout }}
+      breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+      cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+      rowHeight={30}
+      containerPadding={[10, 10]}
+      verticalCompact={true}
+      compactType="vertical"
+      isDraggable={true}
+      isResizable={true}
+      autoSize={true}
+      measureBeforeMount={false}
+      preventCollision={false}
+      onLayoutChange={handleLayoutChange}>
+      {layout.map(item => (
+        <div key={item.i} className="bg-gray-200 dark:bg-gray-400 border-2 border-gray-400 p-4 shadow" id={`block-${item.i}`}>
+          Block {item.i.toUpperCase()}
+        </div>
+      ))}
+    </ResponsiveGridLayout>
+  );
+};
 
 export default MyGridLayout;
