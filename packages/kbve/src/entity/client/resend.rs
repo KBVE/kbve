fbab616972e10713e9d38ff36a56278a150b3505
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-05-08 22:20:23 -0400

    Preparing the Resend API

---
+++ packages/kbve/src/entity/client/resend.rs
@@ -0,0 +1,25 @@
+use reqwest::header::{ HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE };
+use serde_json::json;
+use std::error::Error;
+use crate::db::Pool;
+use std::sync::{ Arc };
+
+use crate::runes::{ WizardResponse };
+
+use axum::{
+  async_trait,
+  http::{ StatusCode, Request, header },
+  extract::{ Extension, Path, State, FromRequest },
+  response::{ IntoResponse, Response },
+  middleware::{ self, Next },
+  Json,
+  BoxError,
+};
+
+pub async fn resend_email(
+  Extension(pool): Extension<Arc<Pool>>,
+  Json(mut body): Json<AuthPlayerRegisterSchema>
+) -> impl IntoResponse {
+
+    
+}
