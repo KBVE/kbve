998a500ac4fc271b9fd78d621049011113e21b9a
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-05-29 20:59:41 +0000

    Preparing the Astropad

---
+++ packages/astropad/index.ts
@@ -0,0 +1,4 @@
+// Do not write code directly here, instead use the `src` folder!
+
+// @ts-ignore TypeScript doesn't know about Astro files
+export { default as Astropad } from './src/Astropad.astro';
