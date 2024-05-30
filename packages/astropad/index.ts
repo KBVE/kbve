4fa5cbeb38564a04676b4ec69af4b011d4ad986a
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-05-30 00:09:16 +0000

    Updated the Resume Store within AstroPad

--- packages/astropad/index.ts
+++ packages/astropad/index.ts
@@ -2,3 +2,5 @@
 
 // @ts-ignore TypeScript doesn't know about Astro files
 export { default as Astropad } from './src/Astropad.astro';
+
+export { default as AstroPadResume } from './src/resume/AstroPadResume.astro';
