8fb26e009a9014e30260539330020d0f4a80a085
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-07-04 01:26:03 -0400

    Added QuadTree into the Laser Package!

--- packages/laser/src/index.ts
+++ packages/laser/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/laser';
+export * from './lib/quadtree';
---
+++ packages/laser/src/lib/quadtree.ts
@@ -0,0 +1,143 @@
+// quadtree.ts
+
+interface Bounds {
+  xMin: number;
+  xMax: number;
+  yMin: number;
+  yMax: number;
+}
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+interface Range {
+  name: string;
+  bounds: Bounds;
+  action: () => void;
+}
+
+class Quadtree {
+  private bounds: Bounds;
+  private capacity: number;
+  private points: Range[];
+  private divided: boolean;
+  private cache: Map<string, Range[]>;
+  private northeast?: Quadtree;
+  private northwest?: Quadtree;
+  private southeast?: Quadtree;
+  private southwest?: Quadtree;
+
+  constructor(bounds: Bounds, capacity = 4) {
+    this.bounds = bounds;
+    this.capacity = capacity;
+    this.points = [];
+    this.divided = false;
+    this.cache = new Map(); // Initialize the cache
+  }
+
+  private subdivide() {
+    const { xMin, yMin, xMax, yMax } = this.bounds;
+    const w = (xMax - xMin) / 2;
+    const h = (yMax - yMin) / 2;
+    const x = xMin;
+    const y = yMin;
+
+    this.northeast = new Quadtree({ xMin: x + w, xMax: x + 2 * w, yMin: y, yMax: y + h }, this.capacity);
+    this.northwest = new Quadtree({ xMin: x, xMax: x + w, yMin: y, yMax: y + h }, this.capacity);
+    this.southeast = new Quadtree({ xMin: x + w, xMax: x + 2 * w, yMin: y + h, yMax: y + 2 * h }, this.capacity);
+    this.southwest = new Quadtree({ xMin: x, xMax: x + w, yMin: y + h, yMax: y + 2 * h }, this.capacity);
+
+    this.divided = true;
+  }
+
+  public insert(range: Range): boolean {
+    if (!this.contains(range.bounds)) {
+      return false;
+    }
+
+    if (this.points.length < this.capacity) {
+      this.points.push(range);
+      return true;
+    } else {
+      if (!this.divided) {
+        this.subdivide();
+      }
+
+      if (this.northeast?.insert(range)) return true;
+      if (this.northwest?.insert(range)) return true;
+      if (this.southeast?.insert(range)) return true;
+      if (this.southwest?.insert(range)) return true;
+    }
+
+    return false; // If none of the insertions succeed
+  }
+
+  private contains(bounds: Bounds): boolean {
+    const { xMin, yMin, xMax, yMax } = this.bounds;
+    return (
+      bounds.xMin >= xMin &&
+      bounds.xMax <= xMax &&
+      bounds.yMin >= yMin &&
+      bounds.yMax <= yMax
+    );
+  }
+
+  private generateCacheKey(range: Bounds): string {
+    const { xMin, xMax, yMin, yMax } = range;
+    return `${xMin},${xMax},${yMin},${yMax}`;
+  }
+
+  public query(point: Point, found: Range[] = []): Range[] {
+    const cacheKey = `${point.x},${point.y}`;
+
+    const cachedResult = this.cache.get(cacheKey);
+    if (cachedResult) {
+      return cachedResult;
+    }
+
+    if (!this.intersects({ xMin: point.x, xMax: point.x, yMin: point.y, yMax: point.y })) {
+      return found;
+    }
+
+    for (const p of this.points) {
+      if (isWithinRange(point, p.bounds)) {
+        found.push(p);
+      }
+    }
+
+    if (this.divided) {
+      this.northwest?.query(point, found);
+      this.northeast?.query(point, found);
+      this.southwest?.query(point, found);
+      this.southeast?.query(point, found);
+    }
+
+    // Store the result in the cache
+    this.cache.set(cacheKey, found);
+
+    return found;
+  }
+
+  private intersects(range: Bounds): boolean {
+    const { xMin, yMin, xMax, yMax } = this.bounds;
+    return !(
+      range.xMin > xMax ||
+      range.xMax < xMin ||
+      range.yMin > yMax ||
+      range.yMax < yMin
+    );
+  }
+}
+
+function isWithinRange(point: Point, bounds: Bounds): boolean {
+  return (
+    point.x >= bounds.xMin &&
+    point.x <= bounds.xMax &&
+    point.y >= bounds.yMin &&
+    point.y <= bounds.yMax
+  );
+}
+
+export { Quadtree, Bounds, Point, Range, isWithinRange };
