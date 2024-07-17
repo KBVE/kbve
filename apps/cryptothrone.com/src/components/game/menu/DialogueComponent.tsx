472348b2ce0e374a01e1d1e1dee1c6f2723d8ff1
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-07-17 18:38:08 -0400

    Fixed Fudster's Avatar!

--- apps/cryptothrone.com/src/components/game/menu/DialogueComponent.tsx
+++ apps/cryptothrone.com/src/components/game/menu/DialogueComponent.tsx
@@ -79,7 +79,7 @@ const DialogueComponent: React.FC = () => {
     <>
       <div
         id="hs-stacked-overlays-dialogue"
-        className="hs-overlay hs-overlay-backdrop-open:bg-gray-900/50 size-full fixed top-0 start-0 z-[60] overflow-x-hidden overflow-y-auto pointer-events-none [--overlay-backdrop:static]"
+        className="hs-overlay hs-overlay-backdrop-open:bg-gray-900/50  fixed top-0 start-0 z-[60] overflow-x-hidden overflow-y-auto pointer-events-none [--overlay-backdrop:static]"
         data-hs-overlay-keyboard="false"
       >
         <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto md:min-w-fit">
