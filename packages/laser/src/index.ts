856c95dfea981b9ca177028850bd3b59f8a3805f
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-07-22 16:00:01 -0400

    Updated the Loader
    Laser package now has the loader class.

--- packages/laser/src/index.ts
+++ packages/laser/src/index.ts
@@ -13,6 +13,7 @@ export * from './lib/localdb';
 // Utils
 export * from './lib/utils/ulid'; // ULID
 export * from './lib/utils/debug'; // Debug
+export * from './lib/utils/loader'; // Loader
 
 // Icons
 export { default as CollapseIcon } from './lib/icon/CollapseIcon';
