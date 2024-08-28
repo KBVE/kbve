eea52e23afa660fe8c8de58b4e77ec5c9b0290a3
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-08-28 18:38:18 +0200

    fix: metro configuration was missing.

--- apps/expo-cityvote/metro.config.js
+++ apps/expo-cityvote/metro.config.js
@@ -39,12 +39,12 @@ mergedConfig = mergeConfig(mergedConfig, tamaguiConfig);
 
 
 
-module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
-  // Change this to true to see debugging info.
-  // Useful if you have issues resolving modules
-  debug: false,
-  // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
-  extensions: [],
-  // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
-  watchFolders: [],
+module.exports = withNxMetro(withTamagui(mergedConfig, {
+  components: ['tamagui'],
+  config: './tamagui.config.ts',
+  outputCSS: './tamagui-web.css',
+}), {
+  debug: false, // Change this to true to see debugging info.
+  extensions: [], // All the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
+  watchFolders: [], // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
 });
