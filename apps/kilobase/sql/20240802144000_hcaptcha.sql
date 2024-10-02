0fc4d90795ed16173a69dcab7e1da440a6504f9f
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-10-02 00:43:31 -0400

    feat(kilobase): additional queries for the failed verification.

--- apps/kilobase/sql/20240802144000_hcaptcha.sql
+++ apps/kilobase/sql/20240802144000_hcaptcha.sql
@@ -8,18 +8,33 @@ CREATE TABLE IF NOT EXISTS public.registration_failed_verification_attempts (
 --- Row Level Security
 ALTER TABLE public.registration_failed_verification_attempts ENABLE ROW LEVEL SECURITY;
 
--- Policy to allow only users with the role 'supabase_auth_admin' to access the table
-CREATE POLICY "Allow supabase_auth_admin access" 
-ON public.registration_failed_verification_attempts
-USING (current_setting('role') = 'supabase_auth_admin');
-
--- Policy to allow the row owner (based on user_email) to view their own failed attempts
-CREATE POLICY "Allow row owner access"
+-- Policy to allow all users to see their own rows
+CREATE POLICY "Allow row owner to view" 
 ON public.registration_failed_verification_attempts
 USING (current_setting('request.jwt.claims.email') = user_email);
 
--- Policy to allow 'supabase_auth_admin' role to insert failed attempts
-CREATE POLICY "Allow supabase_auth_admin insert"
+-- Policy to allow only authenticated users to insert their own email in the table
+CREATE POLICY "Allow insert if user_email matches"
 ON public.registration_failed_verification_attempts
 FOR INSERT
+WITH CHECK (current_setting('request.jwt.claims.email') = user_email);
+
+-- Policy to allow admin role to view all rows
+CREATE POLICY "Allow admin to view all rows"
+ON public.registration_failed_verification_attempts
 USING (current_setting('role') = 'supabase_auth_admin');
+
+-- Policy to allow admin to insert any email
+CREATE POLICY "Allow admin to insert any email"
+ON public.registration_failed_verification_attempts
+FOR INSERT
+WITH CHECK (current_setting('role') = 'supabase_auth_admin');
+
+-- Constraint on emails that are being added.
+ALTER TABLE public.registration_failed_verification_attempts
+ADD CONSTRAINT valid_email_format CHECK (
+  user_email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$' AND
+  char_length(user_email) < 32
+);
+
+-- Hook Function
