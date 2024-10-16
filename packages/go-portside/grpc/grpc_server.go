23c3a55c22a4063e5055819db5652484c30d44f6
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-10-16 00:42:54 -0400

    fix(go-portside): updating the protobufs

--- packages/go-portside/grpc/grpc_server.go
+++ packages/go-portside/grpc/grpc_server.go
@@ -1,32 +1,33 @@
 package grpc
 
 import (
-    "context"
-    "fmt"
-    "net"
+	"context"
+	"fmt"
+	"net"
 
-    pb "path_to_your_proto_package"
-    "google.golang.org/grpc"
+	pb "packages/go-portside/proto"
+
+	"google.golang.org/grpc"
 )
 
 type server struct {
-    pb.UnimplementedMultiplayerServiceServer
+	pb.UnimplementedMultiplayerServiceServer
 }
 
 func (s *server) SendPlayerData(ctx context.Context, req *pb.PlayerRequest) (*pb.PlayerResponse, error) {
-    fmt.Printf("Received data: %+v\n", req)
-    return &pb.PlayerResponse{Status: "OK", Message: "Data received"}, nil
+	fmt.Printf("Received data: %+v\n", req)
+	return &pb.PlayerResponse{Status: "OK", Message: "Data received"}, nil
 }
 
 func StartGRPCServer(address string) error {
-    lis, err := net.Listen("tcp", address)
-    if err != nil {
-        return err
-    }
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return err
+	}
 
-    grpcServer := grpc.NewServer()
-    pb.RegisterMultiplayerServiceServer(grpcServer, &server{})
+	grpcServer := grpc.NewServer()
+	pb.RegisterMultiplayerServiceServer(grpcServer, &server{})
 
-    fmt.Printf("Starting gRPC server at %s\n", address)
-    return grpcServer.Serve(lis)
+	fmt.Printf("Starting gRPC server at %s\n", address)
+	return grpcServer.Serve(lis)
 }
