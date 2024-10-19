f85453be048b30bc99aeec9267515f8aa2da7cb6
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-10-18 20:10:03 -0400

    fix(expo-lcagents): reverting the portal provider.

--- apps/expo-lcagents/index.js
+++ apps/expo-lcagents/index.js
@@ -1,15 +1,7 @@
 import { ExpoRoot } from 'expo-router';
 import 'expo-router/entry';
-import { PortalProvider } from '@tamagui/portal';
-
-
 
 export default function App() {
-  const ctx = require.context('./src/app');
-
-  return (
-    <PortalProvider shouldAddRootHost>
-      <ExpoRoot context={ctx} />
-    </PortalProvider>
-  );
+	const ctx = require.context('./src/app');
+	return <ExpoRoot context={ctx} />;
 }
