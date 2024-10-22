908b5191f16a034807ff30318d8405aac7271871
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-10-22 02:16:34 -0400

    fix(expo-lcagents): login complete!

---
+++ apps/expo-lcagents/src/app/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'tamagui';
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>This is the Consulting Page! TODO: Load Different Projects! Minor Change to Trigger Pipeline Round 5. Force Deployment! Side note, need to add a couple buttons here.</Text>
+    </View>
+  );
+};
+
+export default Profile;
--- packages/expo-bbq/src/index.ts
+++ packages/expo-bbq/src/index.ts
@@ -4,6 +4,7 @@ export { InstaCard } from './components/card/InstaCard';
 export { BBQEmbed } from './components/BBQEmbed';
 export { TamaSimpleCardList } from './components/TamaSimpleCardList';
 export { TamaRegister } from './components/auth/TamaRegister';
+export { TamaLogin } from './components/auth/TamaLogin';
 export { LottieAnimation } from './components/animation/LottieAnimation';
 export { LottieHero } from './components/animation/LottieHero';
 export { MaskedView } from './components/creative/MaskedView';
