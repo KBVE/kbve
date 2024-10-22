de48aade885a6e31bad1a5ecf5af5741ffd35c99
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-10-21 23:29:15 -0400

    feat(kilobase): updating the sql query for user profile.
    [Bug]: Postgres User Profile - username conflict. #3077

--- apps/kilobase/sql/user_profiles/20240903144000_enable_user_profile.sql
+++ apps/kilobase/sql/user_profiles/20240903144000_enable_user_profile.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS public.user_profiles (
 
     -- Constraints for username
     CONSTRAINT username_length CHECK (char_length(username) >= 5 AND char_length(username) <= 24),
-    CONSTRAINT username_format CHECK (username ~ '^[a-zA-Z0-9_-]+$')
+    CONSTRAINT username_format CHECK (username ~ '^[a-z0-9_-]+$')
 );
 
 ALTER TABLE public.user_profiles
@@ -22,7 +22,6 @@ ALTER TABLE public.user_profiles
 DROP POLICY IF EXISTS "Public user_profiles are viewable by everyone." ON public.user_profiles;
 CREATE POLICY "Public user_profiles are viewable by everyone." ON public.user_profiles
     FOR SELECT USING (true);
-
 
 DROP POLICY IF EXISTS "Users can insert their own profile." ON public.user_profiles;
 CREATE POLICY "Users can insert their own profile." ON public.user_profiles
@@ -35,7 +34,7 @@ CREATE POLICY "Users can update own profile." ON public.user_profiles
 -- Drop the existing trigger if it exists
 DROP TRIGGER IF EXISTS handle_user_profiles_update ON public.user_profiles;
 
--- Create the hander user profile update trigger again
+-- Create the handler user profile update trigger again
 CREATE TRIGGER handle_user_profiles_update
     BEFORE UPDATE ON public.user_profiles
     FOR EACH ROW
@@ -48,7 +47,7 @@ CREATE OR REPLACE FUNCTION public.handle_new_user_profile()
         v_username TEXT;
     BEGIN
         -- Assign variables from user meta data
-        v_username := new.raw_user_meta_data->>'username';
+        v_username := lower(new.raw_user_meta_data->>'username'); -- Convert username to lowercase
 
         -- Validate the username
         IF v_username IS NULL OR 
@@ -63,11 +62,9 @@ CREATE OR REPLACE FUNCTION public.handle_new_user_profile()
             RAISE EXCEPTION 'username_taken';
         END IF;
 
-       
-
         -- Insert into user_profiles table
         BEGIN
-            INSERT INTO public.user_profiles (id, username, avatar_url)
+            INSERT INTO public.user_profiles (id, username)
             VALUES (
                 new.id,
                 v_username
@@ -88,7 +85,6 @@ CREATE TRIGGER on_auth_user_created
     AFTER INSERT ON auth.users
     FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user_profile();
 
-
 -- Function to handle profile updates and validation
 CREATE OR REPLACE FUNCTION public.handle_profile_update()
     RETURNS TRIGGER AS $$
@@ -96,10 +92,10 @@ DECLARE
     v_new_username TEXT;
 BEGIN
     -- Assign variables for new username
-    v_new_username := new.username;
+    v_new_username := lower(new.username); -- Ensure lowercase for uniqueness check
 
     -- Validate the new username if it is being changed
-    IF v_new_username IS DISTINCT FROM old.username THEN
+    IF v_new_username IS DISTINCT FROM lower(old.username) THEN
         IF v_new_username IS NULL OR 
            char_length(v_new_username) < 5 OR 
            char_length(v_new_username) > 24 OR 
@@ -116,7 +112,6 @@ BEGIN
     RETURN new;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
-
 
 -- Create or replace the trigger for handling profile updates
 DROP TRIGGER IF EXISTS handle_user_profile_update ON public.user_profiles;
