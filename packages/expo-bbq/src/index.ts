b600f60a9f97dbea3baefdf3028147965c7122a4
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-10-25 06:17:25 -0400

    feat(expo-bbq): started general user cards component.

--- packages/expo-bbq/src/index.ts
+++ packages/expo-bbq/src/index.ts
@@ -3,13 +3,17 @@ export { TamaCard } from './components/TamaCard';
 export { InstaCard } from './components/card/InstaCard';
 export { BBQEmbed } from './components/BBQEmbed';
 export { TamaSimpleCardList } from './components/TamaSimpleCardList';
-export { TamaRegister } from './components/auth/TamaRegister';
-export { TamaLogin } from './components/auth/TamaLogin';
-export { TamaProfile } from './components/auth/TamaProfile';
 export { LottieAnimation } from './components/animation/LottieAnimation';
 export { LottieHero } from './components/animation/LottieHero';
 export { MaskedView } from './components/creative/MaskedView';
 export { useBBQ } from './core/BBQ';
+
+//  [AUTH]
+export { TamaRegister } from './components/auth/TamaRegister';
+export { TamaLogin } from './components/auth/TamaLogin';
+export { TamaProfile } from './components/auth/TamaProfile';
+export { TamaUserCard } from './components/auth/user/TamaUserCard';
+
 //  [PANELS]
 export { TamaSheet } from './components/panels/TamaSheet';
 export { TamaSkeleton } from './components/panels/TamaSkeleton';
