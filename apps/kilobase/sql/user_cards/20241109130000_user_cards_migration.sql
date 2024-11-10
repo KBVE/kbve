819a360faaff310c7b93aae960c010840d69b959
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-11-10 10:20:49 -0500

    fix(kilobase): user_card sql drops were out of order.

--- apps/kilobase/sql/user_cards/20241109130000_user_cards_migration.sql
+++ apps/kilobase/sql/user_cards/20241109130000_user_cards_migration.sql
@@ -1,9 +1,11 @@
 BEGIN;
 
---  Drop older trigger and function
+-- Drop the triggers first
 DROP TRIGGER IF EXISTS on_auth_new_card_created ON auth.users;
+DROP TRIGGER IF EXISTS handle_user_card_update ON public.user_cards;
+
+-- Then drop the functions
 DROP FUNCTION IF EXISTS public.handle_user_card_update();
 DROP FUNCTION IF EXISTS public.handle_new_user_card();
-DROP TRIGGER IF EXISTS handle_user_card_update ON public.user_cards;
 
 COMMIT;
