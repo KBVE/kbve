19c8e73202aed14666ac33cebc4ce2c06af03d93
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-11-10 10:18:37 -0500

    fix(kilobase): updating the user cards to be managed directly via RLS.

--- apps/kilobase/sql/user_cards/20241109130000_user_cards_migration.sql
+++ apps/kilobase/sql/user_cards/20241109130000_user_cards_migration.sql
@@ -3,5 +3,7 @@ BEGIN;
 --  Drop older trigger and function
 DROP TRIGGER IF EXISTS on_auth_new_card_created ON auth.users;
 DROP FUNCTION IF EXISTS public.handle_user_card_update();
+DROP FUNCTION IF EXISTS public.handle_new_user_card();
+DROP TRIGGER IF EXISTS handle_user_card_update ON public.user_cards;
 
 COMMIT;
