4c8171484c85bd563914f5c804856db051c3aef1
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-11-22 01:47:15 -0500

    feat(kilonet): added the messages file for the protobuf.

---
+++ packages/kilonet/kilonet/Networks/ChatMessages.cs
@@ -0,0 +1,10 @@
+namespace KBVE.Kilonet.Networks
+{
+  [Serializable]
+  public class ChatMessage
+  {
+    public string SenderId { get; set; }
+    public string RecipientId { get; set; }
+    public string Content { get; set; }
+  }
+}
