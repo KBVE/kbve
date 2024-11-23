02032ba7b2b5038dbf647fefcaaa8739b717f741
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-11-23 16:40:02 -0500

    fix(mmextensions): added the onMMEvent function.

--- packages/mmextensions/mmextensions/ObjectPool/NetworkObjectPool.cs
+++ packages/mmextensions/mmextensions/ObjectPool/NetworkObjectPool.cs
@@ -10,26 +10,39 @@ using UnityEngine.Tilemaps;
 
 namespace KBVE.MMExtensions.ObjectPool
 {
-  public class NetworkObjectPool : MMMultipleObjectPooler, MMEventListener<TopDownEngineEvent> {
-
-
-        /// <summary>
-        /// Called when the object is destroyed.
-        /// </summary>
-        private void OnDestroy()
-        {
-            Debug.Log("NetworkObjectPool is being destroyed!");
-            Owner?.Remove(this);
-        }
-
-        protected virtual void OnEnable()
-        {
-        this.MMEventStartListening<TopDownEngineEvent>();
-        }
-
-        protected virtual void OnDisable()
-        {
-        this.MMEventStopListening<TopDownEngineEvent>();
-        }
+  public class NetworkObjectPool : MMMultipleObjectPooler, MMEventListener<TopDownEngineEvent>
+  {
+    /// <summary>
+    /// Called when the object is destroyed.
+    /// </summary>
+    private void OnDestroy()
+    {
+      Debug.Log("NetworkObjectPool is being destroyed!");
+      Owner?.Remove(this);
     }
+
+    protected virtual void OnEnable()
+    {
+      this.MMEventStartListening<TopDownEngineEvent>();
+    }
+
+    protected virtual void OnDisable()
+    {
+      this.MMEventStopListening<TopDownEngineEvent>();
+    }
+
+    public virtual void OnMMEvent(TopDownEngineEvent topDownEngineEvent)
+    {
+      switch (topDownEngineEvent.EventType)
+      {
+        case TopDownEngineEventTypes.SpawnComplete:
+          Debug.Log("[NetworkObjectPool] Player transform assigned.");
+          break;
+
+        default:
+          Debug.Log($"[NetworkObjectPool] Unhandled event type: {topDownEngineEvent.EventType}");
+          break;
+      }
+    }
+  }
 }
