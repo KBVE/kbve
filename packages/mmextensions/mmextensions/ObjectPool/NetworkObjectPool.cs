56a6cd34db0d0297f7f222da388391b930306fb2
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-11-23 16:32:20 -0500

    feat(mmextensions): adding the base object pool.
    Co-authored-by: Mike Evans <JigSawPsycho@users.noreply.github.com>
    Co-authored-by: Brad <BChip@users.noreply.github.com>

---
+++ packages/mmextensions/mmextensions/ObjectPool/NetworkObjectPool.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using MoreMountains.Tools;
+using MoreMountains.TopDownEngine;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.Tilemaps;
+
+namespace KBVE.MMExtensions.ObjectPool
+{
+  public class NetworkObjectPool : MMMultipleObjectPooler, MMEventListener<TopDownEngineEvent> {
+
+
+        /// <summary>
+        /// Called when the object is destroyed.
+        /// </summary>
+        private void OnDestroy()
+        {
+            Debug.Log("NetworkObjectPool is being destroyed!");
+            Owner?.Remove(this);
+        }
+
+        protected virtual void OnEnable()
+        {
+        this.MMEventStartListening<TopDownEngineEvent>();
+        }
+
+        protected virtual void OnDisable()
+        {
+        this.MMEventStopListening<TopDownEngineEvent>();
+        }
+    }
+}
