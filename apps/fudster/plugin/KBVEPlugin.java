f376c4a128bf0eea95577bfa0ec96d79a398de8d
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-12-13 15:37:30 -0500

    fix(fudster): updating the plugin / script.

--- apps/fudster/plugin/KBVEPlugin.java
+++ apps/fudster/plugin/KBVEPlugin.java
@@ -18,7 +18,8 @@ import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.config.ProfileManager;
 import net.runelite.client.game.WorldService;
 
-
+//  [KBVE]
+import net.runelite.client.plugins.microbot.kbve.json.KBVEPluginHelper;
 
 //  [Micro]
 import net.runelite.client.plugins.microbot.Microbot;
@@ -55,6 +56,9 @@ public class KBVEPlugin extends Plugin {
     private ProfileManager profileManager;
     @Inject
     private WorldService worldService;
+    @Inject
+    private KBVEPluginHelper kbvePluginHelper;
+
     
     @Provides
     KBVEConfig provideConfig(ConfigManager configManager){
@@ -77,6 +81,7 @@ public class KBVEPlugin extends Plugin {
                 kbveScripts.run(config);
                 Microbot.log("Python!");
             default:
+                String gpuStatusJson = kbvePluginHelper.managePlugin("disable", "GpuPlugin");
                 kbveScripts.run(config);
                 Microbot.log("Default Chan!");
         }
