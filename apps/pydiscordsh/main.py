66d1381cd21cf7297cd206fc50bc4d0a46706242
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-01-08 00:47:02 -0500

    fix(pydiscordsh): added back the lifespan of the database.

--- apps/pydiscordsh/main.py
+++ apps/pydiscordsh/main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI, WebSocket
 from pydiscordsh import Routes, CORS, TursoDatabase
+from contextlib import asynccontextmanager
 
 import logging
 
 logger = logging.getLogger("uvicorn")
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("[DB]@PENDING")
+    await database.start_client()
+    yield
+    logger.info("[DB]@DISINT")
+    await database.stop_client()
+    logger.info("[DB]@STOPPING")
+
+app = FastAPI(lifespan=lifespan)
 routes = Routes(app, templates_dir="templates")
 CORS(app)
 
 database = TursoDatabase()
+
 
 @app.get("/v1/db/start_client")
 async def start_client():
