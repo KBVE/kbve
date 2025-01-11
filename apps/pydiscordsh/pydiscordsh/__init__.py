cd8901918b52a8d48aa335bea591228ce8975c0e
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-01-10 20:41:05 -0500

    refactor(pydiscordsh): quick changes from the previous.

--- apps/pydiscordsh/pydiscordsh/__init__.py
+++ apps/pydiscordsh/pydiscordsh/__init__.py
@@ -1,3 +1,4 @@
 from .api import Routes, CORS
 from .apps import TursoDatabase, DiscordServerManager, Kilobase
 from .api import SetupSchema, Hero, DiscordServer, Health, SchemaEngine, Utils
+from .models import SanitizedBaseModel
