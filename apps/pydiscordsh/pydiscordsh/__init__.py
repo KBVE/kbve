05a34733b9dc01c7bddbcc4c4c72fcbec8d324ec
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-01-12 04:03:14 -0500

    feat(pydiscordsh): added the profile and public user cards.

--- apps/pydiscordsh/pydiscordsh/__init__.py
+++ apps/pydiscordsh/pydiscordsh/__init__.py
@@ -1,5 +1,5 @@
 from .api import Routes, CORS
-from .apps import TursoDatabase, DiscordServerManager, Kilobase, DiscordRouter, DiscordTagManager, TagStatus
+from .apps import TursoDatabase, DiscordServerManager, Kilobase, DiscordRouter, DiscordTagManager, TagStatus, UserManager
 from .api import SetupSchema, Hero, DiscordServer, Health, SchemaEngine, Utils
 from .models import SanitizedBaseModel, DiscordCategories
-from .routes import get_database, get_tag_manager, lifespan, tags_router
+from .routes import get_database, get_tag_manager, lifespan, tags_router, users_router
