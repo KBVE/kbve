5ccbe4cd1c15c033856ec8d4aa8e637a1abebf7a
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-01 17:22:49 -0500

    feat(tower-defense): added a timer extension class

---
+++ apps/rust_godot_towerdefense/src/extensions/timer_extension.rs
@@ -0,0 +1,37 @@
+use godot::prelude::*;
+use godot::classes::Timer;
+
+pub trait TimerExt {
+  fn with_name(self, name: &str) -> Self;
+  fn with_wait_time(self, time: f64) -> Self;
+  fn with_one_shot(self, one_shot: bool) -> Self;
+  fn with_autostart(self, autostart: bool) -> Self;
+  fn with_paused(self, paused: bool) -> Self;
+}
+
+impl TimerExt for Gd<Timer> {
+  fn with_name(mut self, name: &str) -> Self {
+    self.set_name(name);
+    self
+  }
+
+  fn with_wait_time(mut self, time: f64) -> Self {
+    self.set_wait_time(time);
+    self
+  }
+
+  fn with_one_shot(mut self, one_shot: bool) -> Self {
+    self.set_one_shot(one_shot);
+    self
+  }
+
+  fn with_autostart(mut self, autostart: bool) -> Self {
+    self.set_autostart(autostart);
+    self
+  }
+
+  fn with_paused(mut self, paused: bool) -> Self {
+    self.set_paused(paused);
+    self
+  }
+}
