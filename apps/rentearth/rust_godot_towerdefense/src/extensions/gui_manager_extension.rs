45566cea7ca723e0e5d18a1aee14202036300c74
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-14 16:50:16 -0500

    feat(tower-defense): updating the gui ext to include a with_windowflag function.

--- apps/rentearth/rust_godot_towerdefense/src/extensions/gui_manager_extension.rs
+++ apps/rentearth/rust_godot_towerdefense/src/extensions/gui_manager_extension.rs
@@ -6,21 +6,27 @@ use godot::classes::window::Flags as WindowFlags;
 use crate::macos::macos_gui_options::enable_mac_transparency;
 
 pub trait GUIManagerExt {
-    fn with_transparency(&mut self);
+  fn with_transparency(&mut self);
+  fn with_windowflag(&mut self, flag: WindowFlags, flag_boolean: bool);
 }
 
-
 impl GUIManagerExt for Gd<CanvasLayer> {
-    fn with_transparency(&mut self) {
-        if let Some(mut viewport) = self.get_viewport() {
-            viewport.set_transparent_background(true);
-        }
-        if let Some(mut window) = self.get_window() {
-            window.set_flag(WindowFlags::ALWAYS_ON_TOP, true);
-        }
-        #[cfg(target_os = "macos")]
-        {
-            enable_mac_transparency();
-        }
+  fn with_transparency(&mut self) {
+    if let Some(mut viewport) = self.get_viewport() {
+      viewport.set_transparent_background(true);
     }
+    if let Some(mut window) = self.get_window() {
+      window.set_flag(WindowFlags::ALWAYS_ON_TOP, true);
+    }
+    #[cfg(target_os = "macos")]
+    {
+      enable_mac_transparency();
+    }
+  }
+
+  fn with_windowflag(&mut self, flag: WindowFlags, flag_boolean: bool) {
+    if let Some(mut window) = self.get_window() {
+      window.set_flag(flag, flag_boolean);
+    }
+  }
 }
