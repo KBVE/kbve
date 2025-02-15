fad16ac829ca4650d7cd71659f684c6e93f951ec
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-15 00:44:23 -0500

    feat(tower-defense): add wry_extension.rs for the browser support

---
+++ apps/rentearth/rust_godot_towerdefense/src/extensions/wry_extension.rs
@@ -0,0 +1,4 @@
+use godot::classes::display_server::HandleType;
+use godot::classes::DisplayServer;
+use godot::global::godot_print;
+use raw_window_handle::{AppKitWindowHandle, HandleError, HasWindowHandle, RawWindowHandle, Win32WindowHandle, WindowHandle, XlibWindowHandle};
