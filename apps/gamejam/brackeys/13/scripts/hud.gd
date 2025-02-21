025efe0ba82b106b3e4ebb9ed0fc5c4fa2850b16
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-20 20:28:20 -0500

    feat(brackeys13): updated hud with new icons.

--- apps/gamejam/brackeys/13/scripts/hud.gd
+++ apps/gamejam/brackeys/13/scripts/hud.gd
@@ -1,13 +1,24 @@
 extends Control
 
-@onready var score_label = $Panel/Menu/Label
-@onready var name_label = $Panel/Menu/StarshipName
-@onready var stone_label = $Panel/Menu/Stone
-@onready var metal_label = $Panel/Menu/Metal
-@onready var gems_label = $Panel/Menu/Gems
-@onready var gold_label = $Panel/Menu/Gold
+@onready var score_label = $Panel/Menu/Nav/Label
+@onready var name_label = $Panel/Menu/Nav/StarshipName
+
+# Resources
+
+@onready var gold_label = $Panel/Menu/ResourceBar/GoldBox/GoldLabel
+@onready var stone_label = $Panel/Menu/ResourceBar/StoneBox/StoneLabel
+@onready var metal_label = $Panel/Menu/ResourceBar/MetalBox/MetalLabel
+@onready var gems_label = $Panel/Menu/ResourceBar/GemsBox/GemsLabel
+
+@onready var gold_icon = $Panel/Menu/ResourceBar/GoldBox/GoldIcon
+@onready var stone_icon = $Panel/Menu/ResourceBar/StoneBox/StoneIcon
+@onready var metal_icon = $Panel/Menu/ResourceBar/MetalBox/MetalIcon
+@onready var gems_icon = $Panel/Menu/ResourceBar/GemsBox/GemsIcon
+
 
 var resource_labels = {}
+var resource_icons = {}
+
 
 func _ready():
 	resource_labels = {
@@ -15,6 +26,13 @@ func _ready():
 		"stone": stone_label,
 		"metal": metal_label,
 		"gems": gems_label
+	}
+
+	resource_icons = {
+		"gold": gold_icon,
+		"stone": stone_icon,
+		"metal": metal_icon,
+		"gems": gems_icon
 	}
 	
 	call_deferred("_update_starship_name")
@@ -32,11 +50,13 @@ func _update_starship_resources():
 	for resource in Global.resources_list:
 		var amount = Global.get_resource(resource)
 		if resource_labels.has(resource) and resource_labels[resource]:
-			resource_labels[resource].text = "%s: %d" % [resource.capitalize(), amount]
+			#resource_labels[resource].text = "%s: %d" % [resource.capitalize(), amount]
+			resource_labels[resource].text = "%d" % [amount]
 
 func _on_resource_changed(resource_name: String, new_value: int):
 		if resource_labels.has(resource_name) and resource_labels[resource_name]:
-			resource_labels[resource_name].text = "%s: %d" % [resource_name.capitalize(), new_value]
+			#resource_labels[resource_name].text = "%s: %d" % [resource_name.capitalize(), new_value]
+			resource_labels[resource_name].text = "%d" % [new_value]
 
 func _update_starship_name():
 	var starship_name = Global.get_starship_data("name")
