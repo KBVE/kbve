323cb379fdbf13b1e948ba1fc66cf1fc637fa8ab
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-21 05:02:30 -0500

    feat(brackeys13): better entity management.

--- apps/gamejam/brackeys/13/scripts/entity.gd
+++ apps/gamejam/brackeys/13/scripts/entity.gd
@@ -3,41 +3,56 @@ extends Node2D
 const ASTEROID_SCENE = preload("res://scenes/asteroid.tscn")
 
 var asteroid_pool: Array = []
-var active_asteroids: Array = []
 var max_asteroids := int(Global.get_environment_data("asteroids"))
 var spawn_timer: Timer
 
-
-func _ready():
+func start_spawn():
 	spawn_timer = Timer.new()
 	spawn_timer.wait_time = 2.0
 	spawn_timer.one_shot = false
 	add_child(spawn_timer)
 	spawn_timer.connect("timeout", Callable(self, "_spawn_asteroid"))
 	spawn_timer.start()
-	initialize_pool(max_asteroids)
+	Global.connect("entity_destroyed", Callable(self, "_on_entity_destroyed"))
 
 func initialize_pool(size: int):
 	for i in range(size):
 		var asteroid = ASTEROID_SCENE.instantiate()
-		asteroid.connect("tree_exited", Callable(self, "_on_asteroid_destroyed").bind(asteroid))
 		asteroid.visible = false
 		asteroid_pool.append(asteroid)
 		add_child(asteroid)
 
 func _spawn_asteroid():
-	if asteroid_pool.size() > 0:
-		var asteroid = asteroid_pool.pop_back()
-		var screen_size = get_viewport().get_visible_rect().size
-		asteroid.global_position = Vector2(randf_range(0, screen_size.x), randf_range(0, screen_size.y))
-		asteroid.visible = true
-		active_asteroids.append(asteroid)
-	else:
-		#Global.emit_signal("notification_received", "asteroids_gone", "Radar says no asteroids", "info")
-		print("No asteroids available in pool!")
+	for asteroid in asteroid_pool:
+		if not asteroid.visible:
+			var screen_size = get_viewport().get_visible_rect().size
+			var spawn_margin = 50
+
+			var spawn_side = randi_range(0, 3)
+			var spawn_position = Vector2()
+
+			match spawn_side:
+				0: spawn_position = Vector2(randf_range(0, screen_size.x), -spawn_margin)  # Top
+				1: spawn_position = Vector2(randf_range(0, screen_size.x), screen_size.y + spawn_margin)  # Bottom
+				2: spawn_position = Vector2(-spawn_margin, randf_range(0, screen_size.y))  # Left
+				3: spawn_position = Vector2(screen_size.x + spawn_margin, randf_range(0, screen_size.y))  # Right
+
+			var target_position = screen_size / 2.0 + Vector2(randf_range(-100, 100), randf_range(-100, 100))
+			asteroid.movement_vector = (target_position - spawn_position).normalized()
+
+			asteroid.global_position = spawn_position
+			asteroid.visible = true
+			return
+
+	print("No inactive asteroids available!")
 
 func _on_asteroid_destroyed(asteroid):
 	asteroid.visible = false
 	asteroid.global_position = Vector2(-1000, -1000)
-	active_asteroids.erase(asteroid)
-	asteroid_pool.append(asteroid)
+	print("Asteroid returned to pool")
+
+func _on_entity_destroyed(entity_type: String, entity_id: int, additional_data: Dictionary):
+	if entity_type == "asteroid":
+		var asteroid = instance_from_id(entity_id)
+		if asteroid:
+			_on_asteroid_destroyed(asteroid)
--- apps/gamejam/brackeys/13/scripts/game.gd
+++ apps/gamejam/brackeys/13/scripts/game.gd
@@ -3,13 +3,16 @@ extends Node2D
 @onready var spaceship = $Spaceship
 @onready var projectiles = $Projectiles
 @onready var background = $Background
-#@onready var tm = $ToastManager
+@onready var entity = $Entity
 
 const LASER_POOL_SIZE = 10
 
 func _ready():
 	spaceship.connect("laser_shot", _on_spaceship_laser_shot)
 	projectiles.initialize_pool(LASER_POOL_SIZE)
+	entity.initialize_pool(int(Global.get_environment_data("asteroids")))
+	entity.start_spawn()
+
 	Global.emit_signal("notification_received", "game_start", "Game Started! Ready for launch.", "info")
 
 func _on_spaceship_laser_shot(scope_position: Vector2, rotation: float):
