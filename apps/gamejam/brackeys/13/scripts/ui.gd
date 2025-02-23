984d4766aca19413c24cbc687e6c5d6f74966b5c
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-23 02:21:27 -0500

    fix(brackeys13): adding the shop and ui manager to the global.

---
+++ apps/gamejam/brackeys/13/scripts/ui.gd
@@ -0,0 +1,12 @@
+# v2 - UI Manager.
+extends Node
+
+# const UI_SCENE = preload("res://v2/scenes/ui/ui.tscn")
+# const SHOP_SCENE = preload("res://scenes/shop_ui.tscn")
+var ui_instance: Node
+var interaction_prompt: Control
+var shop_ui: Control
+
+func _ready():
+    pass;
+    
