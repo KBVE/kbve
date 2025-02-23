eab5bc9257b65d73f98753dee820ead5428fc968
Al Justin <aljustin.lagarde12@gmail.com>
2025-02-23 09:51:12 +0800

    feat: update theme, put up a direction on how planet open the shop

--- apps/gamejam/brackeys/13/scripts/shop.gd
+++ apps/gamejam/brackeys/13/scripts/shop.gd
@@ -36,159 +36,164 @@ var is_on_confirm := false
 var chosen_upgrade
 var shown_text
 var weapons := [
-    {"name": "+1 laser speed", "stat_name": "laser_speed", "value": 1.0, "cost": 
-        {"stone": 200.0, "metal": 50.0}},
-    {"name": "+1 ammo count", "stat_name": "laser_ammo", "value": 1.0, "cost": 
-        {"gems": 10, "gold": 5}}
+	{"name": "+1 laser speed", "stat_name": "laser_speed", "value": 1.0, "cost": 
+		{"stone": 200.0, "metal": 50.0}},
+	{"name": "+1 ammo count", "stat_name": "laser_ammo", "value": 1.0, "cost": 
+		{"gems": 10, "gold": 5}}
 ]
 var shield := [
-    {"name": "None", "stat_name": "none", "value": 1.0, "cost":
-        {}}
+	{"name": "None", "stat_name": "none", "value": 1.0, "cost":
+		{}}
 ]
 var power := [
-    {"name": "Overheat?", "stat_name": "overheat", "value": 1.0, "cost":
-        {}}
+	{"name": "Overheat?", "stat_name": "overheat", "value": 1.0, "cost":
+		{}}
 ]
 var thrusters := [
-    {"name": "+1 acceleration", "stat_name": "acceleration", "value": 1.0, "cost":
-        {}},
-    {"name": "+1 rotation speed", "stat_name": "rotation_speed", "value": 1.0, "cost":
-        {}}
+	{"name": "+1 acceleration", "stat_name": "acceleration", "value": 1.0, "cost":
+		{}},
+	{"name": "+1 rotation speed", "stat_name": "rotation_speed", "value": 1.0, "cost":
+		{}}
 ]
 var chosen = [weapons, shield, power, thrusters]
 
 
 func _ready():
-    update_resource_panel()
-    update_bottom_panel_screen()
+	update_resource_panel()
+	update_bottom_panel_screen()
 
 #region LeftPanelRegion
 
 func _on_left_button_pressed_left_panel() -> void:
-    if cate_selected == categories[0]:
-        pass
-    else:
-        current_category -= 1
-    update_categories()
+	if cate_selected == categories[0]:
+		pass
+	else:
+		current_category -= 1
+	update_categories()
 
 func _on_right_button_pressed_left_panel() -> void:
-    if cate_selected == categories[3]:
-        pass
-    else:
-        current_category += 1
-    update_categories()
+	if cate_selected == categories[3]:
+		pass
+	else:
+		current_category += 1
+	update_categories()
 
 func update_categories() -> void:
-    cate_selected = categories[current_category]
-    categories_label.text = cate_selected
+	cate_selected = categories[current_category]
+	categories_label.text = cate_selected
 
 func _on_enter_button_pressed_left_panel() -> void:
-    shown_text = chunk_array(chosen[current_category], 4)
-    unshow_confirm_upgrade() # unshow when the confirm text when switching categories
-    update_parts()
+	shown_text = chunk_array(chosen[current_category], 4)
+	unshow_confirm_upgrade() # unshow when the confirm text when switching categories
+	update_parts()
 
 func update_parts() -> void:
-    center_label_left.text = shown_text[current_parts][0].name + "\n" + shown_text[current_parts][1].name
-    center_label_right.text = shown_text[current_parts][2].name + "\n" + shown_text[current_parts][3].name
+	center_label_left.text = shown_text[current_parts][0].name + "\n" + shown_text[current_parts][1].name
+	center_label_right.text = shown_text[current_parts][2].name + "\n" + shown_text[current_parts][3].name
 
 func chunk_array(base: Array, t_size: int) -> Array:
-    var result = []
-    for i in range(0, base.size(), t_size):
-        var chunk = base.slice(i, i + t_size)  # Get up to 'size' elements
-        while chunk.size() < t_size:  # Fill missing spots with "empty"
-            chunk.append({"name": "", "stat": "none", "value": 0.0})
-        result.append(chunk)
-    return result
+	var result = []
+	for i in range(0, base.size(), t_size):
+		var chunk = base.slice(i, i + t_size)  # Get up to 'size' elements
+		while chunk.size() < t_size:  # Fill missing spots with "empty"
+			chunk.append({"name": "", "stat": "none", "value": 0.0})
+		result.append(chunk)
+	return result
 
 #endregion
 
 #region Center Panel
 
 func _on_button_1_left_pressed() -> void:
-    show_confirm_upgrade(0)
+	show_confirm_upgrade(0)
 
 
 func _on_button_2_left_pressed() -> void:
-    show_confirm_upgrade(1)
+	show_confirm_upgrade(1)
 
 
 func _on_button_3_left_pressed() -> void:
-    if shown_text != null:
-        current_parts -= 1
-        current_parts = clamp(current_parts, 0, shown_text.size() - 1)
-        update_parts()
+	if shown_text != null:
+		current_parts -= 1
+		current_parts = clamp(current_parts, 0, shown_text.size() - 1)
+		update_parts()
 
 
 func _on_button_right_pressed() -> void:
-    if is_on_confirm: # Confirm upgrade
-        if can_afford():
-            Global.apply_starship_bonus(chosen_upgrade.stat_name, chosen_upgrade.value)
-            update_resource_panel()
-            update_bottom_panel_screen()
-            print("bought")
-            green_light.texture = green_light_on
-            await get_tree().create_timer(1.0).timeout
-            green_light.texture = green_light_off
-        else:
-            red_light.texture = red_light_on
-            await get_tree().create_timer(1.0).timeout
-            red_light.texture = red_light_off            
-    else:
-        show_confirm_upgrade(2)
+	if is_on_confirm: # Confirm upgrade
+		if can_afford():
+			Global.apply_starship_bonus(chosen_upgrade.stat_name, chosen_upgrade.value)
+			update_resource_panel()
+			update_bottom_panel_screen()
+			print("bought")
+			green_light.texture = green_light_on
+			await get_tree().create_timer(1.0).timeout
+			green_light.texture = green_light_off
+		else:
+			red_light.texture = red_light_on
+			await get_tree().create_timer(1.0).timeout
+			red_light.texture = red_light_off            
+	else:
+		show_confirm_upgrade(2)
 
 func can_afford() -> bool:
-    var cost = chosen_upgrade.get("cost", {})
-    for resource in cost.keys():
-        if Global.resources.get(resource, 0) < cost[resource]:
-            return false  # Not enough resources
-    return true  # Upgrade is affordable
+	var cost = chosen_upgrade.get("cost", {})
+	for resource in cost.keys():
+		if Global.resources.get(resource, 0) < cost[resource]:
+			return false  # Not enough resources
+	return true  # Upgrade is affordable
 
 
 func _on_button_2_right_pressed() -> void:
-    if is_on_confirm: # Cancel upgrade
-        unshow_confirm_upgrade()
-        update_parts()
-    else:
-        show_confirm_upgrade(3)
+	if is_on_confirm: # Cancel upgrade
+		unshow_confirm_upgrade()
+		update_parts()
+	else:
+		show_confirm_upgrade(3)
 
 
 func _on_button_right_3_pressed() -> void:
-    if shown_text != null:
-        current_parts += 1
-        current_parts = clamp(current_parts, 0, shown_text.size() - 1)
-        update_parts()
+	if shown_text != null:
+		current_parts += 1
+		current_parts = clamp(current_parts, 0, shown_text.size() - 1)
+		update_parts()
 
 func show_confirm_upgrade(button: int) -> void:
-    if shown_text != null:
-        if shown_text[current_parts][button].name != "":
-            is_on_confirm = true
-            chosen_upgrade = shown_text[current_parts][button]
-            center_label_left.text = chosen_upgrade.name
-            center_label_right.text = "Confirm" + "\n" + "Cancel"
-            center_label_bottom.text = ""
-            button_1_left.disabled = true
-            button_2_left.disabled = true
-            button_3_left.disabled = true
-            button_3_right.disabled = true
+	if shown_text != null:
+		if shown_text[current_parts][button].name != "":
+			is_on_confirm = true
+			chosen_upgrade = shown_text[current_parts][button]
+			center_label_left.text = chosen_upgrade.name
+			center_label_right.text = "Confirm" + "\n" + "Cancel"
+			center_label_bottom.text = ""
+			button_1_left.disabled = true
+			button_2_left.disabled = true
+			button_3_left.disabled = true
+			button_3_right.disabled = true
 
 func unshow_confirm_upgrade() -> void:
-    is_on_confirm = false
-    button_1_left.disabled = false
-    button_2_left.disabled = false
-    button_3_left.disabled = false
-    button_3_right.disabled = false
-    center_label_bottom.text = "Left Right"
+	is_on_confirm = false
+	button_1_left.disabled = false
+	button_2_left.disabled = false
+	button_3_left.disabled = false
+	button_3_right.disabled = false
+	center_label_bottom.text = "Left Right"
 
 #endregion
 
 func update_resource_panel() -> void:
-    var full_text = ""
-    for i in Global.resources.keys():
-        full_text += "%s: %d " % [i.capitalize(), Global.resources[i]]
-    resource_panel.text = full_text
+	var full_text = ""
+	for i in Global.resources.keys():
+		full_text += "%s: %d " % [i.capitalize(), Global.resources[i]]
+	resource_panel.text = full_text
 
 func update_bottom_panel_screen() -> void:
-    var full_text = ""
-    for i in Global.base_starship_stats.keys():
-        full_text += "%s: %d " % [i.capitalize(), Global.base_starship_stats[i] + Global.starship_bonuses[i]]
-    bottom_panel_screen_label.text = full_text
+	var full_text = ""
+	for i in Global.base_starship_stats.keys():
+		full_text += "%s: %d " % [i.capitalize(), Global.base_starship_stats[i] + Global.starship_bonuses[i]]
+	bottom_panel_screen_label.text = full_text
+
+func _on_button_pressed_exit(): # Hides the shop on exit
+		self.hide()
+		get_tree().paused = false
+		pass
