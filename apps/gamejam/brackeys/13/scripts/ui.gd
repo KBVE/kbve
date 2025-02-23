3b5129d136d47d941ae4a8dfc17ac232cb48e8f1
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-23 03:02:52 -0500

    feat(brackeys13): added the interactions and npc scene.

--- apps/gamejam/brackeys/13/scripts/ui.gd
+++ apps/gamejam/brackeys/13/scripts/ui.gd
@@ -1,12 +1,30 @@
 # v2 - UI Manager.
 extends Node
 
-# const UI_SCENE = preload("res://v2/scenes/ui/ui.tscn")
-# const SHOP_SCENE = preload("res://scenes/shop_ui.tscn")
-var ui_instance: Node
-var interaction_prompt: Control
-var shop_ui: Control
+const SHOP_SCENE = preload("res://scenes/shop/shop.tscn")
+var shop_instance: Node
+# var shop_ui: Control
+const INTERACTIONS_UI = preload("res://scenes/ui/interactions.tscn")
+var interactions: Control
+
+# const UI_SCENE = preload("res://scenes/ui/ui.tscn")
+# var ui_instance: Node
+# var interaction_prompt: Control
+
+const NPC_UI = preload("res://scenes/npc/npc.tscn")
+var npc: Control
 
 func _ready():
+    shop_instance = SHOP_SCENE.instantiate()
+    add_child(shop_instance)
+    shop_instance.visible = false
+
+    interactions = INTERACTIONS_UI.instantiate()
+    add_child(interactions)
+    interactions.visible = false
+
+    npc = NPC_UI.instantiate()
+    add_child(npc)
+    npc.visible = false
+
     pass;
-    
