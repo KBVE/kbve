64d802957452684c2b6cc2f1bacb48d895c646ac
Al Justin <aljustin.lagarde12@gmail.com>
2025-02-23 18:02:59 +0800

    fix: removed the creation of the shop in the global.gd since there is already in the game ui

--- apps/gamejam/brackeys/13/scripts/ui.gd
+++ apps/gamejam/brackeys/13/scripts/ui.gd
@@ -1,9 +1,6 @@
 # v2 - UI Manager.
 extends Node
 
-const SHOP_SCENE = preload("res://scenes/shop/shop.tscn")
-var shop_instance: Node
-
 const INTERACTIONS_UI = preload("res://scenes/ui/interactions.tscn")
 var interactions: CanvasLayer
 
@@ -11,25 +8,21 @@ const NPC_UI = preload("res://scenes/npc/npc.tscn")
 var npc: CanvasLayer
 
 func _ready():
-    shop_instance = SHOP_SCENE.instantiate()
-    add_child(shop_instance)
-    shop_instance.visible = false
+	interactions = INTERACTIONS_UI.instantiate()
+	add_child(interactions)
+	interactions.visible = false
 
-    interactions = INTERACTIONS_UI.instantiate()
-    add_child(interactions)
-    interactions.visible = false
+	npc = NPC_UI.instantiate()
+	add_child(npc)
+	npc.visible = false
 
-    npc = NPC_UI.instantiate()
-    add_child(npc)
-    npc.visible = false
-
-    pass;
+	pass;
 
 func show_npc(image: Texture, name: String, text: String):
-    if npc:
-        npc.set_npc_data(image, name, text)
-        npc.visible = true 
+	if npc:
+		npc.set_npc_data(image, name, text)
+		npc.visible = true 
 
 func hide_npc():
-    if npc:
-        npc.hide_npc()
+	if npc:
+		npc.hide_npc()
