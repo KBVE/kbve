727915f16dd3e1602631fadbab04e6e6bb2b154a
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-25 17:58:42 -0500

    feat(expo-hqplan): added nativewind support to hqplan.

--- apps/experimental/expo-hqplan/metro.config.js
+++ apps/experimental/expo-hqplan/metro.config.js
@@ -1,7 +1,9 @@
 const { withNxMetro } = require('@nx/expo');
 const { getDefaultConfig } = require('@expo/metro-config');
 const { mergeConfig } = require('metro-config');
-const { withTamagui } = require('@tamagui/metro-plugin');
+const { withNativeWind } = require('nativewind/metro');
+
+
 
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
@@ -12,7 +14,7 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  *
  * @type {import('metro-config').MetroConfig}
  */
-const customConfig = {
+const customConfig = withNativeWind({
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
   },
@@ -20,14 +22,10 @@ const customConfig = {
     assetExts: assetExts.filter((ext) => ext !== 'svg'),
     sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
   },
-};
+});
 
 module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
-  // Change this to true to see debugging info.
-  // Useful if you have issues resolving modules
   debug: false,
-  // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
   extensions: [],
-  // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
   watchFolders: [],
 });
