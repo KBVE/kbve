424c9c4d1df5d7d306ce93d3c2bbddef0520657d
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-25 02:13:43 -0500

    fix(expo): trying to figure out this error, further changes.

--- apps/cityvote/expo-cityvote/src/app/_layout.tsx
+++ apps/cityvote/expo-cityvote/src/app/_layout.tsx
@@ -1,21 +1,73 @@
-import '../../tamagui-web.css'
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
+} from '@react-navigation/native';
+import { Link, SplashScreen, Stack, usePathname } from 'expo-router';
+import { useColorScheme } from 'react-native';
+import { TamaguiProvider } from 'tamagui';
+import { PortalProvider } from 'tamagui';
 
-import { Stack } from 'expo-router'
-import { TamaguiProvider } from 'tamagui'
+import { Platform } from 'react-native';
 
-import {tamaguiConfig} from '../../tamagui.config'
+import tamaguiConfig from '../../tamagui.config';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 
-export const RootLayout = () => {
+export { ErrorBoundary } from 'expo-router';
 
+export const unstable_settings = {
+	initialRouteName: 'menu',
+};
 
-  return (
-    <TamaguiProvider config={tamaguiConfig}>
-        <Stack>
-          <Stack.Screen name="menu" options={{ presentation: 'modal' }} />
-          <Stack.Screen name="consulting" />
-        </Stack>
-    </TamaguiProvider>
-  )
+if (Platform.OS === 'web') {
+	require('../../tamagui-web.css');
 }
 
-export default RootLayout
+SplashScreen.preventAutoHideAsync();
+
+export function RootLayout() {
+	const colorScheme = useColorScheme();
+
+	const [interLoaded, interError] = useFonts({
+		Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+		InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+	});
+
+	useEffect(() => {
+		if (interLoaded || interError) {
+			SplashScreen.hideAsync();
+		}
+	}, [interLoaded, interError]);
+
+	if (!interLoaded && !interError) {
+		return null;
+	}
+
+	return (
+		<TamaguiProvider
+			config={tamaguiConfig}
+			defaultTheme={colorScheme || 'dark'}>
+			<PortalProvider shouldAddRootHost>
+				<ThemeProvider
+					value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+					<Stack>
+						<Stack.Screen
+							name="menu"
+							options={{
+								presentation: 'modal',
+								animation: 'fade',
+							}}
+						/>
+						<Stack.Screen
+							name="consulting"
+							options={{ animation: 'fade' }}
+						/>
+					</Stack>
+				</ThemeProvider>
+			</PortalProvider>
+		</TamaguiProvider>
+	);
+}
+
+export default RootLayout;
