b729b0dea122cdc22dbbfc41866a04da5c583cb6
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-27 02:36:08 -0500

    fix(expo): more experimental changes with expo.

--- apps/experimental/expo-hqplan/metro.config.js
+++ apps/experimental/expo-hqplan/metro.config.js
@@ -3,10 +3,8 @@ const { getDefaultConfig } = require('@expo/metro-config');
 const { mergeConfig } = require('metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-
-
 const defaultConfig = getDefaultConfig(__dirname);
-const { assetExts, sourceExts } = defaultConfig.resolver;
+const { assetExts, sourceExts, resolveRequest } = defaultConfig.resolver;
 
 /**
  * Metro configuration
@@ -21,8 +19,8 @@ const customConfig = withNativeWind({
   resolver: {
     assetExts: assetExts.filter((ext) => ext !== 'svg'),
     sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
+    resolveRequest: resolveRequest ?? require('metro-resolver').resolve, // Preserve Metro's resolver if it exists
   },
- 
 });
 
 module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
