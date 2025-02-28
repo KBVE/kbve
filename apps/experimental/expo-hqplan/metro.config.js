ff22ef1f06ac4fd0015cd8e146252dc963c9d962
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-02-27 21:08:54 -0500

    docs(javascript): updated the expo metro notes.

--- apps/experimental/expo-hqplan/metro.config.js
+++ apps/experimental/expo-hqplan/metro.config.js
@@ -1,7 +1,7 @@
 const { withNxMetro } = require('@nx/expo');
 const { getDefaultConfig } = require('@expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
-const { mergeConfig } = require("metro-config");
+const { mergeConfig } = require('metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
@@ -13,30 +13,26 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  * @type {import('metro-config').MetroConfig}
  */
 const customConfig = {
-  cacheVersion: "mobile",
-  transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer")
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "cjs", "mjs", "svg"]
-  }
+	cacheVersion: 'mobile',
+	transformer: {
+		babelTransformerPath: require.resolve('react-native-svg-transformer'),
+	},
+	resolver: {
+		assetExts: assetExts.filter((ext) => ext !== 'svg'),
+		sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
+	},
 };
 
 async function createConfig() {
-  return await withNxMetro(mergeConfig(defaultConfig, customConfig), {
-    // Change this to true to see debugging info.
-    // Useful if you have issues resolving modules
-    debug: false,
-    // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
-    extensions: [],
-    // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
-    watchFolders: []
-  }).then((nxConfig) =>
-    withNativeWind(nxConfig, {
-      input: "./src/global.css"
-    })
-  );
+	return await withNxMetro(mergeConfig(defaultConfig, customConfig), {
+		debug: false,
+		extensions: [],
+		watchFolders: [],
+	}).then((nxConfig) =>
+		withNativeWind(nxConfig, {
+			input: './src/global.css',
+		}),
+	);
 }
 
 module.exports = createConfig();
