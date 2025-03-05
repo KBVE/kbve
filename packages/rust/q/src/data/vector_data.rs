5757b17baab7719e953735209ce76e3efb48f704
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-03-05 15:41:45 -0500

    feat(q): serde for vector3, for future vectors.

--- packages/rust/q/src/data/vector_data.rs
+++ packages/rust/q/src/data/vector_data.rs
@@ -39,3 +39,43 @@ pub mod vector2i_serde {
         Ok(Vector2i { x, y })
     }
 }
+
+pub mod vector3_serde {
+    use super::*;
+
+    pub fn serialize<S>(vector: &Vector3, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let tuple = (vector.x, vector.y, vector.z);
+        tuple.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Vector3, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let (x, y, z) = <(f32, f32, f32)>::deserialize(deserializer)?;
+        Ok(Vector3 { x, y, z })
+    }
+}
+
+pub mod vector3i_serde {
+    use super::*;
+
+    pub fn serialize<S>(vector: &Vector3i, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let tuple = (vector.x, vector.y, vector.z);
+        tuple.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Vector3i, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let (x, y, z) = <(i32, i32, i32)>::deserialize(deserializer)?;
+        Ok(Vector3i { x, y, z })
+    }
+}
