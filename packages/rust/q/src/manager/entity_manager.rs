d1a3525cf7217140aa0f20c39ed99c83775984c0
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-03-09 00:05:14 -0500

    refactor(q): renaming hexgrid to ecs manager.

--- packages/rust/q/src/manager/entity_manager.rs
+++ packages/rust/q/src/manager/entity_manager.rs
@@ -1,12 +1,14 @@
+use papaya::HashMap;
 use godot::prelude::*;
 use crate::entity::npc_entity::{ NPCEntity };
 use crate::entity::player_entity::{ PlayerEntity };
+
 
 #[derive(GodotClass)]
 #[class(base = Node)]
 pub struct EntityManager {
   base: Base<Node>,
-  local: Gd<PlayerEntity>,
+  local_player: Gd<PlayerEntity>,
   active_npcs: Vec<Gd<NPCEntity>>,
   npc_pool: HashMap<String, Gd<NPCEntity>>,
 }
