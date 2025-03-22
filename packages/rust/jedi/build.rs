4e56fedf93418586363ec9321d9f0d9cf8efe5b4
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-03-22 06:12:27 -0400

    feat(jedi): adding the temple state management.

---
+++ packages/rust/jedi/build.rs
@@ -0,0 +1,20 @@
+use std::fs;
+use tonic_build;
+
+fn main() {
+    let out_dir = "src/proto";
+
+    fs::create_dir_all(out_dir).unwrap();
+
+    tonic_build::configure()
+        .out_dir(out_dir)
+        .build_client(true)
+        .build_server(enable)
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        // .field_attribute("status.StatusMessage.type", "#[bitflags]")
+        .compile_protos(
+            &["proto/temple.proto", "proto/supabase.proto"],
+            &["proto"],
+        )
+        .expect("Failed to compile Protobuf files");
+}
