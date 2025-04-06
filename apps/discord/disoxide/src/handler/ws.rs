9ac0056eaab3c5dd38f267ed2ba3462905aa7380
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-04-06 06:57:05 -0400

    fix(disoxide): quick tracing for debugging thin client.

--- apps/discord/disoxide/src/handler/ws.rs
+++ apps/discord/disoxide/src/handler/ws.rs
@@ -12,7 +12,7 @@ use tokio::sync::{ oneshot, Mutex };
 // use tokio::sync::broadcast;
 use crate::entity::state::{ AppGlobalState, SharedState };
 use tokio::sync::mpsc::{ channel, Sender };
-
+use jedi::wrapper::ThinWsCommand;
 use jedi::wrapper::{
   add_watch_key,
   build_redis_envelope_from_ws,
@@ -104,10 +104,19 @@ async fn handle_websocket(socket: WebSocket, state: Arc<AppGlobalState>) {
   let mut recv_task = tokio::spawn(async move {
     while let Some(Ok(msg)) = socket_rx.next().await {
       if let Message::Text(text) = msg {
-        let parsed_msg = parse_ws_command(&text).or_else(|_| {
-          match serde_json::from_str::<jedi::wrapper::ThinWsCommand>(&text) {
-            Ok(thin) => Ok(jedi::wrapper::ThinWsCommand::into(thin)),
-            Err(e) => Err(e),
+        
+        let parsed_msg = parse_ws_command(&text).or_else(|e| {
+          tracing::debug!("Fallback to ThinWsCommand after parse_ws_command failed: {}", e);
+        
+          match serde_json::from_str::<ThinWsCommand>(&text) {
+            Ok(thin) => {
+              tracing::debug!("Parsed ThinWsCommand: {:?}", thin);
+              Ok(thin.into())
+            },
+            Err(e) => {
+              tracing::warn!("Failed to parse ThinWsCommand: {}", e);
+              Err(e)
+            }
           }
         });
 
