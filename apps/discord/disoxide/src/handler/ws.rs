dfdf82ab6ea84e7d2bbcb7013821ed4b6e097047
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-04-06 06:38:13 -0400

    fix(disoxide): added the thin websocket layer.

--- apps/discord/disoxide/src/handler/ws.rs
+++ apps/discord/disoxide/src/handler/ws.rs
@@ -104,7 +104,14 @@ async fn handle_websocket(socket: WebSocket, state: Arc<AppGlobalState>) {
   let mut recv_task = tokio::spawn(async move {
     while let Some(Ok(msg)) = socket_rx.next().await {
       if let Message::Text(text) = msg {
-        match parse_ws_command(&text) {
+        let parsed_msg = parse_ws_command(&text).or_else(|_| {
+          match serde_json::from_str::<jedi::wrapper::ThinWsCommand>(&text) {
+            Ok(thin) => Ok(jedi::wrapper::ThinWsCommand::into(thin)),
+            Err(e) => Err(e),
+          }
+        });
+
+        match parsed_msg {
           Ok(ws_msg) => {
             if let Some(key) = extract_watch_command_key(&ws_msg) {
               let key_arc = Arc::<str>::from(key);
