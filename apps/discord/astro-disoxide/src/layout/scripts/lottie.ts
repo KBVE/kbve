c58d07d3c4b67433b4d415ecb9dcff50c2d54b22
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-04-17 14:56:09 -0400

    refactor(astro-disoxide): moving the lottie worker further off the main thread.

--- apps/discord/astro-disoxide/src/layout/scripts/lottie.ts
+++ apps/discord/astro-disoxide/src/layout/scripts/lottie.ts
@@ -2,7 +2,7 @@ import { initCanvasWorker, destroyCanvasWorker } from './client';
 
 export default function RegisterAlpineLottiePanel(Alpine: typeof window.Alpine) {
 	Alpine.data('lottiePanel', () => ({
-		error: null,
+		error: null as string | null,
 		workerReady: false,
 
 		async init() {
@@ -14,7 +14,8 @@ export default function RegisterAlpineLottiePanel(Alpine: typeof window.Alpine) 
 				canvas.className = "w-full h-full";
 				container.appendChild(canvas);
 
-				const lottieUrl = new URL("/assets/json/lottie/animu.lottie", location.origin).toString();
+				//const lottieUrl = new URL("/assets/json/lottie/animu.lottie", location.origin).toString();
+				const lottieUrl = new URL("http://localhost:4321/assets/json/lottie/animu.json").toString();
 				await initCanvasWorker(canvas, lottieUrl);
 				this.workerReady = true;
 			} catch (e) {
