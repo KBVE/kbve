8c40d8e502b3aa28c5dcd98127dcba092b74fd21
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-04-23 16:32:51 -0400

    feat(astro-disoxide): updating i18n before adding back the nanostores specific plugin.

--- apps/discord/astro-disoxide/src/content/config.ts
+++ apps/discord/astro-disoxide/src/content/config.ts
@@ -12,9 +12,25 @@ const applications = defineCollection({ schema: baseSchema });
 const memes = defineCollection({ schema: baseSchema });
 const blog = defineCollection({ schema: baseSchema });
 
+// * i18n
+
+
+export const sidebarSchema = z.object({
+  dashboard: z.string(),
+  servers: z.string(),
+  logs: z.string(),
+  settings: z.string(),
+  logout: z.string()
+});
+
+const sidebar = defineCollection({ schema: sidebarSchema });
+
+
+
 export const collections = {
   guides,
   applications,
   memes,
   blog,
+  sidebar,
 };
