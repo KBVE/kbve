2d07975d73ace3a9d6b5282934d3f8f9dfbeb40f
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-05-08 01:44:18 -0400

    fix(disoxide): finishing the basic redis restful paths.

--- apps/discord/disoxide/src/handler/redis.rs
+++ apps/discord/disoxide/src/handler/redis.rs
@@ -5,17 +5,29 @@ use axum::{
 use axum::Router;
 use axum::routing::post;
 
-use jedi::{wrapper::{redis_key_update_from_get, RedisEnvelope}};
+use axum::http::StatusCode;
+use jedi::{envelope::try_unwrap_payload, wrapper::{redis_key_update_from_get, RedisEnvelope}};
 use jedi::entity::envelope::wrap_hybrid;
 use jedi::proto::jedi::{MessageKind, PayloadFormat, JediEnvelope};
+use serde::{Deserialize, Serialize};
 
-use std::sync::Arc;
+use std::{borrow::Cow, sync::Arc};
 
 use bytes::Bytes;
 
 use crate::entity::state::{SharedState};
 
-use jedi::proto::redis::{SetCommand as RedisSetPayload, GetCommand as RedisGetPayload, RedisResponse, RedisKeyUpdate};
+//use jedi::proto::redis::{SetCommand as RedisSetPayload, GetCommand as RedisGetPayload, RedisResponse, RedisKeyUpdate};
+
+#[derive(Serialize, Deserialize)]
+pub struct RedisPayload<'a> {
+    pub key: Cow<'a, str>,
+    #[serde(default)]
+    pub value: Option<Cow<'a, str>>,
+    #[serde(default)]
+    pub ttl: Option<usize>,
+}
+
 
 fn wrap_redis_json_command_with_meta<T: serde::Serialize>(
     kind: i32,
@@ -25,12 +37,19 @@ fn wrap_redis_json_command_with_meta<T: serde::Serialize>(
     wrap_hybrid(kind, PayloadFormat::Json, payload, metadata)
 }
 
+
 pub async fn redis_set(
     State(state): State<SharedState>,
-    Json(payload): Json<RedisSetPayload>,
+    Json(payload): Json<RedisPayload<'_>>,
 ) -> impl IntoResponse {
-    let cmd = RedisEnvelope::set(payload.key.clone(), payload.value.clone());
-    match state.temple.send_redis(cmd).await {
+    if payload.value.is_none() {
+        return (axum::http::StatusCode::BAD_REQUEST, "Missing value for SET").into_response();
+    }
+
+    let kind = MessageKind::Redis as i32 | MessageKind::Set as i32;
+    let envelope = wrap_redis_json_command_with_meta(kind, &payload, None);
+
+    match state.temple.send_envelope(envelope).await {
         Ok(_) => Json(true).into_response(),
         Err(e) => (axum::http::StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
     }
@@ -39,33 +58,34 @@ pub async fn redis_set(
 
 pub async fn redis_get(
     State(state): State<SharedState>,
-    Json(payload): Json<RedisGetPayload>,
+    Json(payload): Json<RedisPayload<'_>>,
 ) -> impl IntoResponse {
-    let cmd = RedisEnvelope::get(payload.key.clone());
+    let kind = MessageKind::Redis as i32 | MessageKind::Get as i32;
+    let envelope = wrap_hybrid(kind, PayloadFormat::Json, &payload, None);
 
-    match state.temple.send_redis(cmd).await {
-        Ok(resp) => {
-            let update = redis_key_update_from_get(payload.key, Some(resp.value));
-            Json(update).into_response()
+    match state.temple.send_envelope(envelope).await {
+        Ok(env) => {
+            match try_unwrap_payload::<RedisPayload>(&env) {
+                Ok(result) => Json(result).into_response(),
+                Err(e) => (StatusCode::BAD_REQUEST, e.to_string()).into_response(),
+            }
         }
-        Err(_) => {
-            let update = redis_key_update_from_get(payload.key, Option::<String>::None);
-            Json(update).into_response()
-        }
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
     }
 }
 
 pub async fn redis_del(
     State(state): State<SharedState>,
-    Json(payload): Json<RedisGetPayload>,
+    Json(payload): Json<RedisPayload<'_>>,
 ) -> impl IntoResponse {
-    let cmd = RedisEnvelope::del(payload.key.clone());
-    match state.temple.send_redis(cmd).await {
+    let kind = MessageKind::Redis as i32 | MessageKind::Del as i32;
+    let envelope = wrap_redis_json_command_with_meta(kind, &payload, None);
+
+    match state.temple.send_envelope(envelope).await {
         Ok(_) => Json(true).into_response(),
         Err(e) => (axum::http::StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
     }
 }
-
 
 pub fn redis_router() -> Router<SharedState> {
     Router::new()
