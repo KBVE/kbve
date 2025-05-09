4e1d677d7394c2da3da61ec642f0b188e526c1e5
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-05-09 04:29:06 -0400

    fix(astro-disoxide): adding real time streaming demo.

---
+++ apps/discord/astro-disoxide/src/layout/svelte/websocket/FieldEntry.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	let {
+		key,
+		value,
+		updateFieldValue,
+		removeField
+	}: {
+		key: string;
+		value: string;
+		updateFieldValue: (key: string, value: string) => void;
+		removeField: (key: string) => void;
+	} = $props();
+
+	$effect(() => {
+		updateFieldValue(key, value);
+	});
+</script>
+
+<div class="flex items-center gap-2">
+	<span class="text-purple-300 font-mono">{key}</span>
+	<input
+		class="flex-1 rounded-md p-1 bg-gray-200 text-black"
+		placeholder="Value"
+		bind:value />
+	<button
+		onclick={() => removeField(key)}
+		class="text-xs text-red-500">
+		✕
+	</button>
+</div>
