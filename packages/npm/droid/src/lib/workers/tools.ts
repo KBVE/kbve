23b70e102b3b17eb37dcc396d199e03b7a8e4a74
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-05-14 04:20:51 -0400

    feat(droid): adding helper method for rendering the virutal node.

--- packages/npm/droid/src/lib/workers/tools.ts
+++ packages/npm/droid/src/lib/workers/tools.ts
@@ -1,7 +1,35 @@
+import type { VirtualNode } from '../types/modules';
+
 export function dispatchAsync(fn: () => void) {
 	if (typeof queueMicrotask === 'function') {
 		queueMicrotask(fn);
 	} else {
 		setTimeout(fn, 0);
 	}
+}
+
+export function renderVNode(vnode: VirtualNode): HTMLElement {
+	const el = document.createElement(vnode.tag);
+
+	if (vnode.class) el.className = vnode.class;
+
+	if (vnode.attrs) {
+		for (const [k, v] of Object.entries(vnode.attrs)) {
+			el.setAttribute(k, v);
+		}
+	}
+
+	if (vnode.style) {
+		Object.assign(el.style, vnode.style);
+	}
+
+	if (vnode.children) {
+		for (const child of vnode.children) {
+			el.appendChild(
+				typeof child === 'string' ? document.createTextNode(child) : renderVNode(child)
+			);
+		}
+	}
+
+	return el;
 }
