b0aaabe3cf51b74de2b52e361a3c30f6bf5395a6
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-05-14 20:39:26 -0400

    fix(astro-kbve): adjusting the build to allow 9mb cache files.

--- packages/npm/droid/src/lib/droid.ts
+++ packages/npm/droid/src/lib/droid.ts
@@ -1,5 +1,5 @@
 export async function droid(opts?: { canvas?: boolean }): Promise<{ initialized: boolean }> {
 	const { main } = await import('./workers/main');
-	await main(opts);
+	await main();
 	return { initialized: true };
 }
