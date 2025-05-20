5f2cc98445f07abd5c0178a370464c7bdfe8c0b9
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-05-19 20:05:42 -0400

    feat(mmextensions): preparing for the vcontainer + tile baking integration test.

---
+++ packages/unity/mmextensions/mmextensions/Map/Doorway.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace KBVE.MMExtensions.Map
+{
+
+    public class Doorway : MonoBehaviour
+    {
+        public Vector2Int direction;
+        public bool isConnected;
+    }
+}
