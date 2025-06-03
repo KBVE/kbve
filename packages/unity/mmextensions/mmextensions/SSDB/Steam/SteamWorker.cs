bcb5f56e4268b8edf51d2066fa556d6c66eddb9b
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-06-03 03:10:35 -0400

    fix(mmextensions): adjusting the friends list.

--- packages/unity/mmextensions/mmextensions/SSDB/Steam/SteamWorker.cs
+++ packages/unity/mmextensions/mmextensions/SSDB/Steam/SteamWorker.cs
@@ -10,7 +10,6 @@ using Heathen.SteamworksIntegration;
 using Heathen.SteamworksIntegration.API;
 using PlayerLoopTiming = Cysharp.Threading.Tasks.PlayerLoopTiming;
 using SteamAchievements = Heathen.SteamworksIntegration.API.StatsAndAchievements.Client;
-using SteamFriends = Heathen.SteamworksIntegration.API.Friends.Client;
 
 namespace KBVE.MMExtensions.SSDB.Steam
 {
@@ -120,7 +119,9 @@ namespace KBVE.MMExtensions.SSDB.Steam
                 await UniTask.Yield(PlayerLoopTiming.Update);
 
                 Friends.Clear();
-                var friendsList = SteamFriends.GetFriends(Steamworks.EFriendFlags.k_EFriendFlagImmediate);
+                //var friendsList = SteamFriends.GetFriends(Steamworks.EFriendFlags.k_EFriendFlagImmediate);
+
+                var friendsList = UserData.MyFriends;
 
                 foreach (var friend in friendsList)
                 {
