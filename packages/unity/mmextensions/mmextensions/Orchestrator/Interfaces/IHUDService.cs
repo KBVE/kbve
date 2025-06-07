8e8667289f5caa6132ab0eca4af1049cd1147be2
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-06-07 07:24:24 -0400

    feat(mmextensions): adding the HUD service.

--- packages/unity/mmextensions/mmextensions/Orchestrator/Interfaces/IHUDService.cs
+++ packages/unity/mmextensions/mmextensions/Orchestrator/Interfaces/IHUDService.cs
@@ -1,5 +1,6 @@
 using ObservableCollections;
 using KBVE.MMExtensions.Orchestrator.Health;
+using Cysharp.Threading.Tasks;
 
 namespace KBVE.MMExtensions.Orchestrator.Core.UI
 {
@@ -7,7 +8,7 @@ namespace KBVE.MMExtensions.Orchestrator.Core.UI
     {
         ObservableList<StatObservable> ReactiveStats { get; }
 
-        void SetActiveStats(Dictionary<StatType, StatData> statMap);
+        UniTask SetActiveStatsAsync(Dictionary<StatType, StatData> statMap);
         void UpdateStat(StatType type, StatData data);
         StatObservable GetObservable(StatType type);
         void ClearStats();
