657f2500cb759f050bea6a16c29a5b587c57cc39
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-06-10 05:28:57 -0400

    feat(mmextensions): exposing the hudservice panel.

--- packages/unity/mmextensions/mmextensions/Orchestrator/UI/Containers/UIContainerStats.cs
+++ packages/unity/mmextensions/mmextensions/Orchestrator/UI/Containers/UIContainerStats.cs
@@ -4,8 +4,10 @@ using TMPro;
 using R3;
 using KBVE.MMExtensions.Orchestrator.Health;
 using System;
+using VContainer;
 using VContainer.Unity;
 using Cysharp.Threading.Tasks;
+using KBVE.MMExtensions.Orchestrator.Interfaces;
 
 namespace KBVE.MMExtensions.Orchestrator.Core.UI
 {
@@ -13,19 +15,45 @@ namespace KBVE.MMExtensions.Orchestrator.Core.UI
     {
 
         private readonly CompositeDisposable _subscription = new();
+        private readonly ReactiveProperty<bool> _isUIVisible = new(true);
         private CancellationTokenSource _cts;
 
+        private IGlobalCanvas _globalCanvas;
+        private IHUDService _hudService;
 
-
-        public UniTask StartAsync(CancellationToken cancellation = default)
+        [Inject]
+        public void Construct(IGlobalCanvas canvas, IHUDService hudService)
         {
-            throw new NotImplementedException();
+            _globalCanvas = canvas;
+            _hudService = hudService;
         }
+
+        private GameObject _panel1;
+        private GameObject _panel2;
+
+        public async UniTask StartAsync(CancellationToken cancellation = default)
+        {
+            _cts = new CancellationTokenSource();
+            var linkedToken = CancellationTokenSource.CreateLinkedTokenSource(_cts.Token, cancellation).Token;
+            await UniTask.WaitUntil(() => _globalCanvas?.Canvas != null
+            && _hudService?.HUDPanel != null, cancellationToken: cancellation);
+            await Operator.R();
+
+            _isUIVisible.Subscribe(isVisible =>
+            {
+                _panel1.SetActive(isVisible);
+                _panel2.SetActive(isVisible);
+            }).AddTo(_subscription);
+
+            await UniTask.Yield(linkedToken);
+        }
+
         public void Dispose()
         {
             _cts?.Cancel();
             _cts?.Dispose();
             _subscription?.Dispose();
+            _cts = null;
         }
         private void OnDestroy()
         {
