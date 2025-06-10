d5d3864243f460ff1503a33c942a4cc73b6083df
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-06-10 04:02:01 -0400

    feat(mmextensions): adding the iasync startable.

--- packages/unity/mmextensions/mmextensions/Orchestrator/UI/Containers/UIContainerStats.cs
+++ packages/unity/mmextensions/mmextensions/Orchestrator/UI/Containers/UIContainerStats.cs
@@ -4,21 +4,29 @@ using TMPro;
 using R3;
 using KBVE.MMExtensions.Orchestrator.Health;
 using System;
-
+using VContainer.Unity;
+using Cysharp.Threading.Tasks;
 
 namespace KBVE.MMExtensions.Orchestrator.Core.UI
 {
-    public class UIContainerStats : MonoBehaviour, IDisposable
+    public class UIContainerStats : MonoBehaviour, IDisposable, IAsyncStartable
     {
 
-        private readonly CompositeDisposable _subscription = new(); 
+        private readonly CompositeDisposable _subscription = new();
+        private CancellationTokenSource _cts;
 
 
+
+        public UniTask StartAsync(CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
         public void Dispose()
         {
+            _cts?.Cancel();
+            _cts?.Dispose();
             _subscription?.Dispose();
         }
-
         private void OnDestroy()
         {
             Dispose();
