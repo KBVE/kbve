9347be38c9f90690d5a1265061b919d214b87885
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-06-13 07:33:12 -0400

    refactor(droid): a huge mess.

--- packages/npm/droid/src/lib/droid.ts
+++ packages/npm/droid/src/lib/droid.ts
@@ -1,5 +1,5 @@
 export async function droid(opts?: { workerURLs?: Record<string, string> }): Promise<{ initialized: boolean }> {
-	const { main } = await import('./workers/main');
+	const { main } = await import('./workers/main.js');
 	await main({ workerURLs: opts?.workerURLs });
 	return { initialized: true };
 }
