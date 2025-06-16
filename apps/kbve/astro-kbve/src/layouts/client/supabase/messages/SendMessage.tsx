03d5a67c85fd04bd0f38276d8ad02bca5b3a948c
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-06-16 12:01:57 -0400

    feat(sql): preparing the process fees.

--- apps/kbve/astro-kbve/src/layouts/client/supabase/messages/SendMessage.tsx
+++ apps/kbve/astro-kbve/src/layouts/client/supabase/messages/SendMessage.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { transferSchema, transferBalance, type TransferInput } from './transferBalance';
+//import { transferSchema, transferBalance, type TransferInput } from './transferBalance';
 import { useStore } from '@nanostores/react';
 import { userBalanceAtom, userIdAtom } from 'src/layouts/client/supabase/profile/userstate';
 import { clsx, twMerge } from 'src/utils/tw';
