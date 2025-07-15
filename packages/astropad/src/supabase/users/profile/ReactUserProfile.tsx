6aa7a2cd81be1307e0ef23b60a750ef49888ecc6
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-07-14 21:26:11 -0400

    style(astropad): adding the lucide react icons.

--- packages/astropad/src/supabase/users/profile/ReactUserProfile.tsx
+++ packages/astropad/src/supabase/users/profile/ReactUserProfile.tsx
@@ -4,3 +4,11 @@ import { useStore } from '@nanostores/react';
 import { userClientService, supabase } from '@kbve/astropad';
 import { FixedSizeList as List } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
+import {
+  User,
+  BadgeCheck,
+  MailCheck,
+  Link2,
+  ListTree,
+  X
+} from 'lucide-react';
