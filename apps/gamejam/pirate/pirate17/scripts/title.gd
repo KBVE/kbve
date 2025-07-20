90375bd390cb58a2acf21b463cfd0e60e8cd13f8
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-07-20 11:40:33 -0400

    feat(pirate17): adding the player singleton.

--- apps/gamejam/pirate/pirate17/scripts/title.gd
+++ apps/gamejam/pirate/pirate17/scripts/title.gd
@@ -4,8 +4,8 @@ extends Control
 @onready var player_ulid_label = $VBoxContainer/PlayerUlidLabel
 
 func _ready():
-	player_name_label.text = "Welcome, " + Global.player_name + "!"
-	player_ulid_label.text = "ULID: " + Global.player_ulid
+	player_name_label.text = "Welcome, " + Global.player.player_name + "!"
+	player_ulid_label.text = "ULID: " + Global.player.player_ulid
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
