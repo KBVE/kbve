7dbefc8b87874b73f8413bb16bae94f10145e979
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-08-10 19:32:54 -0400

    fix(ssdb): adding more resources for UnitySession.

--- packages/unity/ssdb/ssdb/SupabaseFDW/UnitySession.cs
+++ packages/unity/ssdb/ssdb/SupabaseFDW/UnitySession.cs
@@ -1,6 +1,23 @@
+using KBVE.SSDB;
+using KBVE.SSDB.SupabaseFDW;
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+using R3;
+using Supabase;
+using Supabase.Gotrue;
+using Supabase.Gotrue.Interfaces;
+using Client = Supabase.Client;
 
 
-public class UnitySession: IGotrueSessionPersistence<Session>
+namespace KBVE.SSDB.SupabaseFDW
 {
-    
+    public class UnitySession : IGotrueSessionPersistence<Session>
+    {
+
+    }
+
 }
