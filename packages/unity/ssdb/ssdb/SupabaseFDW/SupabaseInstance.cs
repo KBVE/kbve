214cc2cfcf74f0577c532b90aa725c18cb219131
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-08-10 18:53:41 -0400

    fix(ssdb): updating the perms for SupabaseInfo.

--- packages/unity/ssdb/ssdb/SupabaseFDW/SupabaseInstance.cs
+++ packages/unity/ssdb/ssdb/SupabaseFDW/SupabaseInstance.cs
@@ -1,4 +1,5 @@
 using KBVE.SSDB;
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
--- packages/unity/ssdb/ssdb/Types/SupabaseInfo.cs
+++ packages/unity/ssdb/ssdb/Types/SupabaseInfo.cs
@@ -4,9 +4,9 @@ namespace KBVE.SSDB
 {
     public static class SupabaseInfo
     {
-        const string Url = "https://nyqhblhiffuajvauzeom.supabase.co";
+        public const string Url = "https://nyqhblhiffuajvauzeom.supabase.co";
         //not the API key (api key not needed)
-        const string AnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cWhibGhpZmZ1YWp2YXV6ZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NjE2NzMsImV4cCI6MjA3MDQzNzY3M30.C2Bs0peWleo85-y4tHT7ofLu8JdAbiqAp-PpHq01lTk";
+        public const string AnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cWhibGhpZmZ1YWp2YXV6ZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NjE2NzMsImV4cCI6MjA3MDQzNzY3M30.C2Bs0peWleo85-y4tHT7ofLu8JdAbiqAp-PpHq01lTk";
 
 
     }
