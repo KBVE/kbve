31c8c4655cdf9a54550eeb43c892b25fbca9892b
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-08-23 00:13:48 -0400

    fix(ssdb): updating the supabase public key.

--- packages/unity/ssdb/ssdb/Types/SupabaseInfo.cs
+++ packages/unity/ssdb/ssdb/Types/SupabaseInfo.cs
@@ -4,10 +4,8 @@ namespace KBVE.SSDB
 {
     public static class SupabaseInfo
     {
-        public const string Url = "https://nyqhblhiffuajvauzeom.supabase.co";
-        //not the API key (api key not needed)
-        public const string AnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cWhibGhpZmZ1YWp2YXV6ZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NjE2NzMsImV4cCI6MjA3MDQzNzY3M30.C2Bs0peWleo85-y4tHT7ofLu8JdAbiqAp-PpHq01lTk";
-
+        public const string Url = "https://supabase.kbve.com";
+        public const string AnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzU1NDAzMjAwLCJleHAiOjE5MTMxNjk2MDB9.oietJI22ZytbghFywvdYMSJp7rcsBdBYbcciJxeGWrg";
 
     }
 }
