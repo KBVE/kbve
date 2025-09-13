6e8041b01e5708bfd54346e035253991dc87cd77
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-09-13 18:59:11 -0400

    Trunk/unity ssdb blittable copy (#6350)
    * fix(unity): removing the csproject because we will just use the embed system.
    
    * feat(unity|ssdb): added the blittable copy.
    
    * chore(unity): bumping package versions.

--- packages/unity/ssdb/ssdb/Editor/BlittableCopy.cs
+++ packages/unity/ssdb/ssdb/Editor/BlittableCopy.cs
@@ -1,52 +1,56 @@
-// using System;
-// using System.Collections.Generic;
-// using Puerts;
-// using UnityEngine.UIElements;
-// #if UNITY_EDITOR
-// namespace KBVE.SSDB {
-//     [Configure]
-//     public class PuertsCfg {
-//         [CodeOutputDirectory]
-//         static string OutputDir => UnityEngine.Application.dataPath + "/_gen/";
+using System;
+using System.Collections.Generic;
+using Puerts;
+using UnityEngine.UIElements;
+using OneJS;
+using OneJS.Attributes;
+using OneJS.Dom;
 
-//         // Declare here the C# types that your JS code may reference. 
-//         // Generally, you want to focus on types on hot paths.
-//         [Binding]
-//         static IEnumerable<Type> Bindings {
-//             get {
-//                 return new List<Type>() {
-//                     typeof(UnityEngine.Rect),
-//                     typeof(UnityEngine.Color),
-//                     typeof(UnityEngine.Color32),
-//                     typeof(UnityEngine.Vector2),
-//                     typeof(UnityEngine.Vector3),
-//                     typeof(UnityEngine.Quaternion),
-//                     typeof(VisualElement),
-//                     typeof(MeshGenerationContext),
-//                     typeof(Painter2D),
-//                     typeof(OneJS.Dom.Document),
-//                     typeof(OneJS.Dom.Dom),
-//                     typeof(OneJS.Dom.DomStyle),
-//                 };
-//             }
-//         }
+#if UNITY_EDITOR
+namespace KBVE.SSDB.Editor {
+    [Configure]
+    public class PuertsCfg {
+        [CodeOutputDirectory]
+        static string OutputDir => UnityEngine.Application.dataPath + "/_gen/";
 
-//         // Declare here any struct you'll be using during interop. PuerTS will 
-//         // optimize the memory usage, eliminating GC in most cases.
-//         [BlittableCopy]
-//         static IEnumerable<Type> Blittables {
-//             get {
-//                 return new List<Type>() {
-//                     typeof(UnityEngine.Rect),
-//                     typeof(UnityEngine.Color),
-//                     typeof(UnityEngine.Color32),
-//                     typeof(UnityEngine.Vector2),
-//                     typeof(UnityEngine.Vector3),
-//                     typeof(UnityEngine.Quaternion),
-//                 };
-//             }
-//         }
-//     }
-// }
+        // Declare here the C# types that your JS code may reference. 
+        // Generally, you want to focus on types on hot paths.
+        [Binding]
+        static IEnumerable<Type> Bindings {
+            get {
+                return new List<Type>() {
+                    typeof(UnityEngine.Rect),
+                    typeof(UnityEngine.Color),
+                    typeof(UnityEngine.Color32),
+                    typeof(UnityEngine.Vector2),
+                    typeof(UnityEngine.Vector3),
+                    typeof(UnityEngine.Quaternion),
+                    typeof(VisualElement),
+                    typeof(MeshGenerationContext),
+                    typeof(Painter2D),
+                    typeof(OneJS.Dom.Document),
+                    typeof(OneJS.Dom.Dom),
+                    typeof(OneJS.Dom.DomStyle),
+                };
+            }
+        }
 
-// #endif
+        // Declare here any struct you'll be using during interop. PuerTS will 
+        // optimize the memory usage, eliminating GC in most cases.
+        [BlittableCopy]
+        static IEnumerable<Type> Blittables {
+            get {
+                return new List<Type>() {
+                    typeof(UnityEngine.Rect),
+                    typeof(UnityEngine.Color),
+                    typeof(UnityEngine.Color32),
+                    typeof(UnityEngine.Vector2),
+                    typeof(UnityEngine.Vector3),
+                    typeof(UnityEngine.Quaternion),
+                };
+            }
+        }
+    }
+}
+
+#endif
