ffdde2851d5fb38f176a8f6498318561238544d1
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-09-14 18:16:03 -0400

    feat(unity|ssdb): preparing the supabase bridge.
    Co-authored-by: javissoft01 <javissoft01@users.noreply.github.com>

---
+++ packages/unity/ssdb/ssdb/SupabaseBridge.cs
@@ -0,0 +1,16 @@
+#if !UNITY_WEBGL && !UNITY_IOS && !UNITY_ANDROID
+
+using System;
+using UnityEngine;
+using OneJS;
+using Puerts;
+using R3;
+using VContainer;
+using VContainer.Unity;
+using KBVE.SSDB.SupabaseFDW;
+using Cysharp.Threading.Tasks;
+using System.Threading;
+using MoreMountains.Tools;
+
+
+#endif
