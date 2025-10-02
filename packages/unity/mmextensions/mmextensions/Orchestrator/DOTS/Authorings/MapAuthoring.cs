78d1e087a230fdf9c2d11fee7a75c7b8ffd00b2c
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-10-02 10:59:37 -0400

    refactor(unity|mmext): updating the map authoring to use a resource collection instead of just rock.

--- packages/unity/mmextensions/mmextensions/Orchestrator/DOTS/Authorings/MapAuthoring.cs
+++ packages/unity/mmextensions/mmextensions/Orchestrator/DOTS/Authorings/MapAuthoring.cs
@@ -11,27 +11,40 @@ namespace KBVE.MMExtensions.Orchestrator.DOTS
         {
             public override void Bake(MapAuthoring authoring)
             {
-                if (authoring.RockPrefabs == null)
-                    return;
-
-                var rockCollectionEntity = CreateAdditionalEntity(TransformUsageFlags.None);
-                var rockBuffer = AddBuffer<PrefabLink>(rockCollectionEntity);
-                rockBuffer.Capacity = authoring.RockPrefabs.Length;
-                for (int i = 0; i < authoring.RockPrefabs.Length; i++)
-                    _ = rockBuffer.Add(new PrefabLink { link = GetEntity(authoring.RockPrefabs[i], TransformUsageFlags.None) });
+                // Create resource collection entity if we have resource prefabs
+                Entity resourceCollectionEntity = Entity.Null;
+                if (authoring.ResourcePrefabs != null && authoring.ResourcePrefabs.Length > 0)
+                {
+                    resourceCollectionEntity = CreateAdditionalEntity(TransformUsageFlags.None);
+                    var resourceBuffer = AddBuffer<PrefabLink>(resourceCollectionEntity);
+                    resourceBuffer.Capacity = authoring.ResourcePrefabs.Length;
+                    
+                    for (int i = 0; i < authoring.ResourcePrefabs.Length; i++)
+                    {
+                        if (authoring.ResourcePrefabs[i] != null)
+                        {
+                            _ = resourceBuffer.Add(new PrefabLink 
+                            { 
+                                link = GetEntity(authoring.ResourcePrefabs[i], TransformUsageFlags.Dynamic) 
+                            });
+                        }
+                    }
+                }
 
                 AddComponent(GetEntity(TransformUsageFlags.None), new MapSettings
                 {
-                    rockCollectionLink = rockCollectionEntity,
-                    rockCount = authoring.RockCount,
+                    resourceCollectionLink = resourceCollectionEntity,
+                    resourceCount = authoring.ResourceCount,
                     size = authoring.Rect
                 });
             }
         }
 
-        [FormerlySerializedAs("_gizmoColor ")] public Color GizmoColor = Color.green;
+        [Header("Map Bounds")]
+        [FormerlySerializedAs("_gizmoColor")]
+        public Color GizmoColor = Color.green;
+        
         [Space]
-
         [FormerlySerializedAs("_rect")]
         [Tooltip("Defines the world bounds as (MinXY, MaxXY). Default is -25 to 25 grid.")]
         public float2x2 Rect = new float2x2(
@@ -39,14 +52,27 @@ namespace KBVE.MMExtensions.Orchestrator.DOTS
             new float2(25f, 25f)    // top-right
         );
 
-        [FormerlySerializedAs("_rockCount")] public int RockCount;
-        [FormerlySerializedAs("_rockPrefabs")] public GameObject[] RockPrefabs;
+        [Header("Resources (Rocks, Trees, Ore, etc.)")]
+        [Tooltip("All resource node prefabs to spawn (should have ResourceAuthoring component)")]
+        [FormerlySerializedAs("_rockPrefabs")]
+        public GameObject[] ResourcePrefabs;
+        
+        [Tooltip("Total number of resource nodes to spawn randomly on the map")]
+        [FormerlySerializedAs("_rockCount")]
+        [Min(0)]
+        public int ResourceCount = 100;
 
 #if UNITY_EDITOR
-    private void OnDrawGizmosSelected()
-    {
-        Utils.DrawRect(Rect, GizmoColor);
-    }
+        private void OnDrawGizmosSelected()
+        {
+            Utils.DrawRect(Rect, GizmoColor);
+        }
+
+        private void OnValidate()
+        {
+            if (ResourceCount < 0)
+                ResourceCount = 0;
+        }
 #endif
     }
 }
