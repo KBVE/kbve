a82e0949c77f9469a381e74b4e640980e9556453
Mike Evans <19207822+JigSawPsycho@users.noreply.github.com>
2025-10-12 13:40:42 +1300

    fix: soldiers overlapping when moving

--- packages/unity/mmextensions/mmextensions/Orchestrator/DOTS/Authorings/SoldierAuthoring.cs
+++ packages/unity/mmextensions/mmextensions/Orchestrator/DOTS/Authorings/SoldierAuthoring.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Entities;
+using Unity.Entities;
+using Unity.Physics;
 using UnityEngine;
 
 namespace KBVE.MMExtensions.Orchestrator.DOTS
---
+++ packages/unity/mmextensions/mmextensions/Orchestrator/DOTS/Systems/2DPositionRotationLockingSystem.cs
@@ -0,0 +1,28 @@
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Burst;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public partial struct PositionRotationLocking2DSystem : ISystem
+{
+    [BurstCompile]
+    [WithAll(typeof(PhysicsVelocity))]
+    [WithAll(typeof(LocalTransform))]
+    private partial struct Constrain2DAxisJob : IJobEntity
+    {
+        private void Execute(ref PhysicsVelocity velocity, ref LocalTransform transform)
+        {
+            velocity.Angular = new float3(0f, 0f, 0f);
+            transform.Position.z = 0;
+            transform.Rotation = quaternion.identity;
+        }
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var constrainJob = new Constrain2DAxisJob();
+        state.Dependency = constrainJob.ScheduleParallelByRef(state.Dependency);
+    }
+}
