14c89f79cae29434b6629e6fb52891417c0a1ade
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2026-03-23 04:55:20 -0400

    fix(ows): add missing Microsoft.AspNetCore.Http using for WriteAsync
    Startup.cs uses HttpResponse.WriteAsync() in the /health endpoint but
    was missing the required using directive. Causes CS1061 build failure
    in the Docker publish stage.
    
    Closes #8796

--- apps/ows/ows-management/Startup.cs
+++ apps/ows/ows-management/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
