c2a15a3d64f242004cd07be68c2fe6191be1d889
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2026-04-23 14:23:22 -0400

    style(kbve): adding back a better style of the adsense container to the site.

--- apps/kbve/astro-kbve/src/components/astropad/index.ts
+++ apps/kbve/astro-kbve/src/components/astropad/index.ts
@@ -1,5 +1,6 @@
 export { default as Adsense } from './adsense/Adsense.astro';
 export { default as AdsenseArticle } from './adsense/AdsenseArticle.astro';
+export { default as AdsenseContainer } from './adsense/ContainerAdsense.astro';
 export { default as Giscus } from './giscus/Giscus.astro';
 export { default as DevCode } from './devcode/Devcode.astro';
 export { default as Tasks } from './tasks/Tasks.astro';
