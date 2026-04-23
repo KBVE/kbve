40eb30cefa018f7ca57f13c2f6582003f381cbec
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2026-04-23 12:14:36 -0400

    feat(rareicon): preparing the astro file integrations for the website.

---
+++ apps/rareicon/astro-rareicon/tailwind.config.mjs
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				purple: {
+					50: '#faf5ff',
+					100: '#f3e8ff',
+					200: '#e9d5ff',
+					300: '#d8b4fe',
+					400: '#c084fc',
+					500: '#a855f7',
+					600: '#9333ea',
+					700: '#7e22ce',
+					800: '#6b21a8',
+					900: '#581c87',
+					950: '#3b0764',
+				},
+				primary: {
+					50: '#faf5ff',
+					100: '#f3e8ff',
+					200: '#e9d5ff',
+					300: '#d8b4fe',
+					400: '#c084fc',
+					500: '#a855f7',
+					600: '#9333ea',
+					700: '#7e22ce',
+					800: '#6b21a8',
+					900: '#581c87',
+					950: '#3b0764',
+				},
+			},
+		},
+	},
+	plugins: [],
+};
