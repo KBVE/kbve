a11afead0e67b31c05e627c11171f13bc7e3537d
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2026-05-21 22:59:50 -0400

    deploy(isometric): update WASM build assets

---
+++ apps/kbve/astro-kbve/public/isometric/assets/snippets/bevy_tasker-0dfd933cd86e8647/inline0.js
@@ -0,0 +1,9 @@
+export function atomics_wait_async(buf, index, value) {
+	return Atomics.waitAsync(buf, index, value);
+}
+export function wait_result_async(result) {
+	return result.async;
+}
+export function wait_result_value(result) {
+	return result.value;
+}
