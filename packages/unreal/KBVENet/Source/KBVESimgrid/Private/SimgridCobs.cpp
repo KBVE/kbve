28f54f3406bb085ed742359763343db9ca8c7bc2
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2026-06-30 03:31:27 -0400

    feat(KBVESimgrid): COBS encode/decode with roundtrip test

---
+++ packages/unreal/KBVENet/Source/KBVESimgrid/Private/SimgridCobs.cpp
@@ -0,0 +1,57 @@
+#include "SimgridCobs.h"
+
+TArray<uint8> FSimgridCobs::Encode(const TArray<uint8>& In)
+{
+	TArray<uint8> Out;
+	int32 CodeIdx = Out.Num();
+	Out.Add(0);
+	uint8 Code = 1;
+	for (uint8 B : In)
+	{
+		if (B == 0)
+		{
+			Out[CodeIdx] = Code;
+			CodeIdx = Out.Num();
+			Out.Add(0);
+			Code = 1;
+		}
+		else
+		{
+			Out.Add(B);
+			Code += 1;
+			if (Code == 0xff)
+			{
+				Out[CodeIdx] = Code;
+				CodeIdx = Out.Num();
+				Out.Add(0);
+				Code = 1;
+			}
+		}
+	}
+	Out[CodeIdx] = Code;
+	Out.Add(0);
+	return Out;
+}
+
+bool FSimgridCobs::Decode(const TArray<uint8>& In, TArray<uint8>& Out)
+{
+	Out.Reset();
+	int32 i = 0;
+	while (i < In.Num())
+	{
+		const uint8 Code = In[i++];
+		if (Code == 0)
+		{
+			return true;
+		}
+		for (int32 j = 1; j < Code && i < In.Num(); ++j)
+		{
+			Out.Add(In[i++]);
+		}
+		if (Code != 0xff && i < In.Num() && In[i] != 0)
+		{
+			Out.Add(0);
+		}
+	}
+	return true;
+}
---
+++ packages/unreal/KBVENet/Source/KBVESimgrid/Private/Tests/SimgridCobsTests.cpp
@@ -0,0 +1,28 @@
+#if WITH_DEV_AUTOMATION_TESTS
+
+#include "Misc/AutomationTest.h"
+#include "SimgridCobs.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FSimgridCobsRoundtripTest,
+	"KBVE.Simgrid.Cobs.Roundtrip",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FSimgridCobsRoundtripTest::RunTest(const FString& Parameters)
+{
+	TArray<uint8> Payload = { 0x11, 0x00, 0x00, 0x22, 0x33 };
+	TArray<uint8> Encoded = FSimgridCobs::Encode(Payload);
+	TestEqual("delimiter terminated", Encoded.Last(), (uint8)0x00);
+
+	TArray<uint8> Decoded;
+	const bool bOk = FSimgridCobs::Decode(Encoded, Decoded);
+	TestTrue("decode ok", bOk);
+	TestEqual("roundtrip length", Decoded.Num(), Payload.Num());
+	for (int32 i = 0; i < Payload.Num(); ++i)
+	{
+		TestEqual(FString::Printf(TEXT("byte %d"), i), Decoded[i], Payload[i]);
+	}
+	return true;
+}
+
+#endif
