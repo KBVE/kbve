57aebff16da3818d2d5eb3c0dce6ffb6f682f679
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2026-07-01 02:09:14 -0400

    feat(KBVESimgrid): ephemeral projectile/equipped/stats/inventory decoders

--- packages/unreal/KBVENet/Source/KBVESimgrid/Private/Tests/SimgridEphemeralTests.cpp
+++ packages/unreal/KBVENet/Source/KBVESimgrid/Private/Tests/SimgridEphemeralTests.cpp
@@ -71,4 +71,74 @@ bool FSimgridEphemeralStatusTest::RunTest(const FString& Parameters)
 	return true;
 }
 
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FSimgridEphemeralProjectileTest,
+	"KBVE.Simgrid.Ephemeral.Projectile",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FSimgridEphemeralProjectileTest::RunTest(const FString& Parameters)
+{
+	const FSimgridProjectile P = FEphemeralCodec::DecodeProjectile(HexBytes(TEXT("020a050e04056172726f7701")));
+	TestEqual("attacker", P.Attacker, (uint32)2);
+	TestEqual("from.x", P.From.X, 5);
+	TestEqual("from.y", P.From.Y, -3);
+	TestEqual("to.x", P.To.X, 7);
+	TestEqual("to.y", P.To.Y, 2);
+	TestEqual("kind", P.Kind, FString(TEXT("arrow")));
+	TestTrue("hit", P.bHit);
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FSimgridEphemeralEquippedTest,
+	"KBVE.Simgrid.Ephemeral.Equipped",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FSimgridEphemeralEquippedTest::RunTest(const FString& Parameters)
+{
+	const FSimgridEquipped E = FEphemeralCodec::DecodeEquipped(HexBytes(TEXT("010573776f726406776561706f6e0602")));
+	TestTrue("has ref", E.bHasItemRef);
+	TestEqual("ref", E.ItemRef, FString(TEXT("sword")));
+	TestEqual("slot", E.Slot, FString(TEXT("weapon")));
+	TestEqual("attack", E.Attack, 3);
+	TestEqual("defense", E.Defense, 1);
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FSimgridEphemeralStatsTest,
+	"KBVE.Simgrid.Ephemeral.Stats",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FSimgridEphemeralStatsTest::RunTest(const FString& Parameters)
+{
+	const FSimgridStats S = FEphemeralCodec::DecodeStats(HexBytes(TEXT("0464c801500e031428")));
+	TestEqual("level", S.Level, 2);
+	TestEqual("xp", S.Xp, 50);
+	TestEqual("xp_next", S.XpNext, 100);
+	TestEqual("max_hp", S.MaxHp, 40);
+	TestEqual("attack", S.Attack, 7);
+	TestEqual("kills", S.Kills, (uint32)3);
+	TestEqual("mp", S.Mp, 10);
+	TestEqual("max_mp", S.MaxMp, 20);
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FSimgridEphemeralInventoryTest,
+	"KBVE.Simgrid.Ephemeral.Inventory",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FSimgridEphemeralInventoryTest::RunTest(const FString& Parameters)
+{
+	const FSimgridInventory Inv = FEphemeralCodec::DecodeInventory(HexBytes(TEXT("0200056172726f77030006706f74696f6e01")));
+	TestEqual("count", Inv.Items.Num(), 2);
+	TestEqual("i0 id", Inv.Items[0].Id, FString(TEXT("")));
+	TestEqual("i0 ref", Inv.Items[0].ItemRef, FString(TEXT("arrow")));
+	TestEqual("i0 count", Inv.Items[0].Count, (uint32)3);
+	TestEqual("i1 ref", Inv.Items[1].ItemRef, FString(TEXT("potion")));
+	TestEqual("i1 count", Inv.Items[1].Count, (uint32)1);
+	return true;
+}
+
 #endif
