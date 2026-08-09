89661304bf93e623898d269ae7e164e81675d080
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2026-08-09 02:24:56 -0400

    fix(friendslop): updating the day and night cycle again.

--- apps/friendslop/godot-friendslop/src/world/day_night.gd
+++ apps/friendslop/godot-friendslop/src/world/day_night.gd
@@ -1,11 +1,28 @@
 extends Node3D
 
+@export_category("Time")
 @export var day_length_minutes := 10.0
 @export var start_hour := 9.0
+
+@export_category("Celestial Movement")
+@export var light_angle_step_deg := 0.15
+
+@export_category("Sun")
+@export var sun_max_energy := 1.0
+@export var sun_shadow_distance := 80.0
+
+@export_category("Moon")
+@export var moon_max_energy := 0.20
+@export var moon_shadow_distance := 45.0
+
+@export_category("Shadow Thresholds")
+@export var sun_shadow_elevation := 0.04
+@export var moon_shadow_elevation := 0.10
 
 var hour: float
 
 var _last_hour := -1
+var _last_angle := INF
 
 @onready var sun: DirectionalLight3D = $Sun
 @onready var moon: DirectionalLight3D = $Moon
@@ -13,7 +30,8 @@ var _last_hour := -1
 
 func _ready() -> void:
 	hour = start_hour
-	_update_lights()
+	_configure_shadows()
+	_update_lights(true)
 
 
 func _process(delta: float) -> void:
@@ -25,9 +43,44 @@ func _process(delta: float) -> void:
 		Game.events.notify(EventNames.HOUR_CHANGED, h)
 
 
-func _update_lights() -> void:
-	var angle := (hour - 6.0) * TAU / 24.0
-	sun.rotation = Vector3(-angle, 0.0, 0.0)
-	moon.rotation = Vector3(-angle + PI, 0.0, 0.0)
-	sun.light_energy = clampf(-(-sun.global_basis.z).y * 3.0, 0.0, 1.0)
-	moon.light_energy = clampf(-(-moon.global_basis.z).y * 3.0, 0.0, 0.35)
+func _configure_shadows() -> void:
+	sun.directional_shadow_mode = DirectionalLight3D.SHADOW_PARALLEL_2_SPLITS
+	moon.directional_shadow_mode = DirectionalLight3D.SHADOW_PARALLEL_2_SPLITS
+	sun.directional_shadow_max_distance = sun_shadow_distance
+	moon.directional_shadow_max_distance = moon_shadow_distance
+	sun.directional_shadow_fade_start = 0.85
+	moon.directional_shadow_fade_start = 0.80
+	sun.directional_shadow_split_1 = 0.18
+	moon.directional_shadow_split_1 = 0.22
+	sun.directional_shadow_blend_splits = false
+	moon.directional_shadow_blend_splits = false
+
+
+func _update_lights(force := false) -> void:
+	var raw_angle := (hour - 6.0) * TAU / 24.0
+	var visual_angle := raw_angle
+	if light_angle_step_deg > 0.0:
+		visual_angle = snappedf(raw_angle, deg_to_rad(light_angle_step_deg))
+
+	var sun_elevation := sin(raw_angle)
+	var moon_elevation := -sun_elevation
+
+	var sun_amount := smoothstep(-0.04, 0.20, sun_elevation)
+	var moon_amount := smoothstep(-0.02, 0.20, moon_elevation)
+	sun.light_energy = sun_amount * sun_max_energy
+	moon.light_energy = moon_amount * moon_max_energy
+
+	var sun_casts_shadow := sun_elevation > sun_shadow_elevation
+	var moon_casts_shadow := moon_elevation > moon_shadow_elevation and not sun_casts_shadow
+	if sun.shadow_enabled != sun_casts_shadow:
+		sun.shadow_enabled = sun_casts_shadow
+	if moon.shadow_enabled != moon_casts_shadow:
+		moon.shadow_enabled = moon_casts_shadow
+
+	sun.shadow_opacity = smoothstep(sun_shadow_elevation, 0.18, sun_elevation)
+	moon.shadow_opacity = smoothstep(moon_shadow_elevation, 0.25, moon_elevation) * 0.55
+
+	if force or not is_equal_approx(visual_angle, _last_angle):
+		_last_angle = visual_angle
+		sun.rotation.x = -visual_angle
+		moon.rotation.x = -visual_angle + PI
