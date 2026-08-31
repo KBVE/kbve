6f9fddff7d3c2571ffa1c6624a615e672459d275
Al @h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2026-08-31 15:29:00 -0400

    feat(discordsh): removing the notification bot.

--- apps/discordsh/notification-bot/notification_bot/__init__.py
+++
@@ -1,7 +0,0 @@
-from .api import CORS  # noqa: F401
-from .api import supabase_conn  # noqa: F401
-from .api import get_supabase_client  # noqa: F401
-from .api import VaultSecretResponse  # noqa: F401
-from .api import VaultOperationResponse  # noqa: F401
-from .api import DiscordBotService  # noqa: F401
-from .utils.dependencies import lifespan  # noqa: F401
--- apps/discordsh/notification-bot/notification_bot/api/discord/embed/__init__.py
+++
@@ -1,2 +0,0 @@
-from .discord_status_embed import BotStatusView  # noqa: F401
-from .discord_status_embed import send_bot_status_embed  # noqa: F401
